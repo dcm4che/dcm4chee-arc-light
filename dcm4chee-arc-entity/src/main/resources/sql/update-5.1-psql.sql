48ea4092e80598b5991544596c43508626bc9418
Gunter Zeilinger <gunterze@gmail.com>
2016-02-25 22:43:20 +0100

    dcm4che/dcm4chee-arc-light#45: Retry to retrieve objects from fallback

--- dcm4chee-arc-entity/src/main/resources/sql/update-5.1-psql.sql
+++ dcm4chee-arc-entity/src/main/resources/sql/update-5.1-psql.sql
@@ -5,6 +5,6 @@ create index UK_q8k2sl3kjl18qg1nr19l47tl1 on study (access_time);
 create index UK_24av2ewa70e7cykl340n63aqd on study (access_control_id);
 alter table code drop constraint UK_sb4oc9lkns36wswku831c33w6;
 alter table code add constraint UK_l01jou0o1rohy7a9p933ndrxg  unique (code_value, code_designator);
-alter table study add failed_retrieves int4, add failed_iuids text;
+alter table study add failed_retrieves int4, add failed_iuids varchar(32768);
 update study set failed_retrieves = 0;
 alter table study alter failed_retrieves set not null;
