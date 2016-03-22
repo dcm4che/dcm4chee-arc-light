10a549aaba331244cc7ccf50f25f3ba39c386307
Shefki Esadi <shefki_esadi@hotmail.de>
2016-03-22 13:35:49 +0100

    Show user name of logged-in user in UI#71

--- dcm4chee-arc-ui/src/main/webapp/js/controllers/mainCtr.js
+++ dcm4chee-arc-ui/src/main/webapp/js/controllers/mainCtr.js
@@ -1,7 +1,8 @@
 "use strict";
 
-myApp.controller('MainController', function ($scope,$location) {
+myApp.controller('MainController', function ($scope, $location, $http) {
   $scope.logoutUrl = myApp.logoutUrl();
+  $scope.showUserMenu = false;
 	$scope.getClass = function (path) {
 		if($location.path().substr(0, path.length) === path) {
 		    return 'active';
@@ -10,6 +11,21 @@ myApp.controller('MainController', function ($scope,$location) {
 		}
 			  
 	};
+  $http({
+      method: 'GET',
+      url: 'rs/realm'
+  }).then(function successCallback(response) {
+    // console.log("response. user=",JSON.parse(response.data));
+    $scope.user = {};
+    $scope.user.username = response.data.user;
+    if(response.data.roles.indexOf("admin") > 0){
+      $scope.user.role = "admin";
+    }else{
+      $scope.user.role = response.data.roles[0];
+    }
+  }, function errorCallback(response) {
+      // vex.dialog.alert("Error loading device names, please reload the page and try again!");
+  }); 
   $scope.toggleMenu = function(){
     if($scope.showMenu){
       	$scope.showMenu = false;
