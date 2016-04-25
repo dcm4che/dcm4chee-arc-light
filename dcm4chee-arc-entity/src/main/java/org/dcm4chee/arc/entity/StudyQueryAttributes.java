6c032e5fee69decc0c7473c4bea424e1484915eb
Gunter Zeilinger <gunterze@gmail.com>
2016-04-25 10:30:51 +0200

    Fix dcm4che/dcm4chee-arc-light#104: Query occasionally returns Retrieve AETs instead SOP Class UIDs in attribute SOPClassesInStudy

--- dcm4chee-arc-entity/src/main/java/org/dcm4chee/arc/entity/StudyQueryAttributes.java
+++ dcm4chee-arc-entity/src/main/java/org/dcm4chee/arc/entity/StudyQueryAttributes.java
@@ -131,7 +131,7 @@ public class StudyQueryAttributes {
     }
 
     public String getRawSOPClassesInStudy() {
-        return retrieveAETs;
+        return sopClassesInStudy;
     }
 
     public String[] getSOPClassesInStudy() {
