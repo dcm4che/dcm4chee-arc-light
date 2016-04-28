c8fb0879d4ca5d54490d8b9cb7ad043eb2356306
vrindanayak <vrinda.nayak@j4care.com>
2016-04-28 13:34:32 +0200

    Update mysql 5.1 script

--- dcm4chee-arc-entity/src/main/resources/sql/update-5.1-mysql.sql
+++ dcm4chee-arc-entity/src/main/resources/sql/update-5.1-mysql.sql
@@ -3,7 +3,7 @@ update study set access_time = updated_time, scattered_storage = false, access_c
 alter table study modify access_time datetime not null, modify scattered_storage bit not null, modify access_control_id varchar(255) not null;
 create index UK_q8k2sl3kjl18qg1nr19l47tl1 on study (access_time);
 create index UK_24av2ewa70e7cykl340n63aqd on study (access_control_id);
-alter table code drop constraint UK_sb4oc9lkns36wswku831c33w6;
+alter table code drop index UK_sb4oc9lkns36wswku831c33w6;
 alter table code add constraint UK_l01jou0o1rohy7a9p933ndrxg  unique (code_value, code_designator);
 alter table study add failed_retrieves integer, add failed_iuids varchar(4000);
 update study set failed_retrieves = 0;
