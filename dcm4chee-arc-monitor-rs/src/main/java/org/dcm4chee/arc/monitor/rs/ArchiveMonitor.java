dff7ecd3968393ead0752e84dc06dd8e18f6563a
Gunter Zeilinger <gunterze@gmail.com>
2016-06-09 16:05:42 +0200

    Fix dcm4che/dcm4chee-arc-light#151: RESTful service to list Open Associations

---
+++ dcm4chee-arc-monitor-rs/src/main/java/org/dcm4chee/arc/monitor/rs/ArchiveMonitor.java
@@ -0,0 +1,103 @@
+/*
+ * *** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is part of dcm4che, an implementation of DICOM(TM) in
+ * Java(TM), hosted at https://github.com/gunterze/dcm4che.
+ *
+ * The Initial Developer of the Original Code is
+ * J4Care.
+ * Portions created by the Initial Developer are Copyright (C) 2013
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ * See @authors listed below
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * *** END LICENSE BLOCK *****
+ */
+
+package org.dcm4chee.arc.monitor.rs;
+
+/**
+ * @author Gunter Zeilinger <gunterze@gmail.com>
+ * @since Jun 2016
+ */
+
+import org.dcm4che3.net.Association;
+import org.dcm4che3.net.Device;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.StreamingOutput;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Path("/monitor")
+@RequestScoped
+public class ArchiveMonitor {
+
+    @Inject
+    private Device device;
+
+    @GET
+    @Path("associations")
+    @Produces("application/json")
+    public StreamingOutput listDevices() throws Exception {
+        return new StreamingOutput() {
+            @Override
+            public void write(OutputStream out) throws IOException {
+                DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS");
+                Writer w = new OutputStreamWriter(out, "UTF-8");
+                int count = 0;
+                w.write('[');
+                for (Association as : device.listOpenAssociations()) {
+                    if (count++ > 0)
+                        w.write(',');
+                    w.write("{\"serialNo\":");
+                    w.write(String.valueOf(as.getSerialNo()));
+                    w.write(",\"connectTime\":\"");
+                    w.write(df.format(new Date(as.getConnectTimeInMillis())));
+                    w.write("\",\"initiated\":");
+                    w.write(String.valueOf(as.isRequestor()));
+                    w.write(",\"localAETitle\":\"");
+                    w.write(as.getLocalAET());
+                    w.write("\",\"remoteAETitle\":\"");
+                    w.write(as.getRemoteAET());
+                    w.write("\"}");
+                }
+                w.write(']');
+                w.flush();
+            }
+        };
+    }
+}
