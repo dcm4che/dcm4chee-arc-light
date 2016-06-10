c7d7bcbddbf7dfffbbd810090a2d33269164832e
Shefki Esadi <shefki_esadi@hotmail.de>
2016-06-10 18:06:41 +0200

    developing loop with dummy data [UI: Show list of Open Associations #152]

--- dcm4chee-arc-ui/src/main/webapp/js/controllers/control.js
+++ dcm4chee-arc-ui/src/main/webapp/js/controllers/control.js
@@ -1,8 +1,11 @@
 "use strict";
 
 myApp.controller('ArchiveCtrl', function ($scope, $http) {
+
+    $scope.updaterate = 10;
     $scope.logoutUrl = myApp.logoutUrl();
     $scope.status = null;
+    $scope.stopLoop = true;
     $scope.message = '';
     $scope.fetchStatus = function() {
         $http.get("../ctrl/status").then(function (res) {
@@ -28,4 +31,39 @@ myApp.controller('ArchiveCtrl', function ($scope, $http) {
         })
     };
     $scope.fetchStatus();
+    $scope.monitor = function(){
+        $scope.stopLoop = false;
+        var dummy = [
+                        {
+                            "serialNo":9,
+                            "connectTime":"2016-06-10T09:56:52.431",
+                            "initiated":false,
+                            "localAETitle":"DCM4CHEE",
+                            "remoteAETitle":"MOVESCU"
+                        },
+                        {
+                            "serialNo":10,
+                            "connectTime":"2016-06-10T09:56:52.503",
+                            "initiated":true,
+                            "localAETitle":"DCM4CHEE",
+                            "remoteAETitle":"STORESCP"
+                        }
+                    ];
+        // $http.get("../ctrl/reload").then(function (res) {
+        //     $scope.message = 'Reload successful';
+        // })
+        $scope.associationStatus = dummy;
+        var associationLoop = setInterval(function () {
+            if ($scope.stopLoop){
+                clearInterval(associationLoop);
+                console.log("stoped");
+            }else{
+                $scope.$apply(function(){
+                    $scope.associationStatus[0].connectTime = $scope.associationStatus[0].connectTime+".";
+                });
+                console.log("running");
+            }
+        }, $scope.updaterate * 1000);
+
+    };
 });
