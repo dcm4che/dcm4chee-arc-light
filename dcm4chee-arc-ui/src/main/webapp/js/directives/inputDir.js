ba839dd5fab753cebd4335229b89a4b297734476
Shefki Esadi <shefki_esadi@hotmail.de>
2016-06-14 11:20:34 +0200

    adding filters, refactorisation[UI: Show list of Open Associations #152]

--- dcm4chee-arc-ui/src/main/webapp/js/directives/inputDir.js
+++ dcm4chee-arc-ui/src/main/webapp/js/directives/inputDir.js
@@ -17,23 +17,25 @@ myApp.directive("input", function ($window, $log, $compile) {
     return{
         restrict: 'E',
         link: function(scope, element, attrs) {
-            angular.element(element).bind("keydown keyup change click",function(){
-                if(angular.element(this).val() != ""){
-                    angular.element(this).siblings("span.x").show();
-                }    
-            });
-            var ngmodel = getScopeModelFromString(scope, attrs.ngModel);
-            if(attrs.type==="text"){
-                var div  = angular.element('<div class="input"></div>');
-                var x    = angular.element('<span class="x glyphicon single_clear '+attrs.id+' glyphicon-remove-sign" ng-hide="'+attrs.ngModel+'===\'\'" ng-click="'+attrs.ngModel+'=\'\'"></span>');
-                div.insertAfter(element);
-                div.append(element);
-                div.append(x);
-                if(ngmodel === ""){
-                    x.hide();
+            if(!(attrs.class && attrs.class.indexOf('no-close-button') > -1)){
+                angular.element(element).bind("keydown keyup change click",function(){
+                    if(angular.element(this).val() != ""){
+                        angular.element(this).siblings("span.x").show();
+                    }    
+                });
+                var ngmodel = getScopeModelFromString(scope, attrs.ngModel);
+                if(attrs.type==="text"){
+                    var div  = angular.element('<div class="input"></div>');
+                    var x    = angular.element('<span class="x glyphicon single_clear '+attrs.id+' glyphicon-remove-sign" ng-hide="'+attrs.ngModel+'===\'\'" ng-click="'+attrs.ngModel+'=\'\'"></span>');
+                    div.insertAfter(element);
+                    div.append(element);
+                    div.append(x);
+                    if(ngmodel === ""){
+                        x.hide();
+                    }
                 }
+                $compile(x)(scope);
             }
-            $compile(x)(scope);
         }
     }
 });
