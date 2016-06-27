6b2f4e01799364abbdbb46e37214c066293962b0
Gunter Zeilinger <gunterze@gmail.com>
2016-06-27 15:38:11 +0200

    dcm4che/dcm4chee-arc-light#182: Study Retention Policy

--- dcm4chee-arc-entity/src/main/resources/sql/update-5.5-psql.sql
+++ dcm4chee-arc-entity/src/main/resources/sql/update-5.5-psql.sql
@@ -1,5 +1,6 @@
-alter table study add exipiration_date varchar(255);
-alter table series add exipiration_date varchar(255);
+alter table study add expiration_date varchar(255);
+alter table series add expiration_date varchar(255);
+create table id_sequence (name varchar(255) not null, next_value int4 not null, version int8, primary key (name));
 
 create index UK_ta3pi6exqft5encv389hwjytw on series (expiration_date);
 create index UK_mlk5pdi8une92kru8g2ppappx on study (expiration_date);
