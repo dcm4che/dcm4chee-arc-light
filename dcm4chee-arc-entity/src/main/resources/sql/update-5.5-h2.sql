e3645f35ff500349436cdcdc54d20aa56a600fe6
vrindanayak <vrinda.nayak@j4care.com>
2016-06-28 14:35:32 +0200

    dcm4che/dcm4chee-arc-light#222 : Update db scripts for 5.5.0 for databases other than postgres

---
+++ dcm4chee-arc-entity/src/main/resources/sql/update-5.5-h2.sql
@@ -0,0 +1,6 @@
+alter table study add expiration_date varchar(255);
+alter table series add expiration_date varchar(255);
+create table id_sequence (name varchar(255) not null, next_value integer not null, version bigint, primary key (name));
+
+create index UK_ta3pi6exqft5encv389hwjytw on series (expiration_date);
+create index UK_mlk5pdi8une92kru8g2ppappx on study (expiration_date);
