3679d49b5155ab5a8e39bbff03e48034d26c5e08
vrindanayak <vrinda.nayak@j4care.com>
2016-07-19 15:40:09 +0200

    dcm4che/dcm4chee-arc-light#267: Store metadata of stored Instances in JSON format on storage

--- dcm4chee-arc-entity/src/main/resources/sql/update-5.6-psql.sql
+++ dcm4chee-arc-entity/src/main/resources/sql/update-5.6-psql.sql
@@ -1,5 +1,5 @@
 alter table location add multi_ref int4, add uidmap_fk int8, add object_type int4;
-update location set obj_type = 0;
+update location set object_type = 0;
 alter table location alter object_type set not null;
 alter table location alter tsuid drop not null;
 create table uidmap (pk int8 not null, uidmap bytea not null, primary key (pk));
