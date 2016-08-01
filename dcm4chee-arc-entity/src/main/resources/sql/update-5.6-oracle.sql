26767742a26754a22e743d7ca6c611555963308d
Gunter Zeilinger <gunterze@gmail.com>
2016-08-01 11:21:14 +0200

    Update db scripts for 5.6.0 for databases other than postgres #270

--- dcm4chee-arc-entity/src/main/resources/sql/update-5.6-oracle.sql
+++ dcm4chee-arc-entity/src/main/resources/sql/update-5.6-oracle.sql
@@ -1,13 +1,12 @@
-alter table location add multi_ref number(10,0), add uidmap_fk number(19,0), add object_type number(10,0);
+alter table patient add num_studies number(10,0);
+update patient set num_studies = (
+  select count(*) from study where study.patient_fk=patient.pk and study.rejection_state in (0,1));
+alter table location add ( multi_ref number(10,0), uidmap_fk number(19,0), object_type number(10,0));
 update location set object_type = 0;
 alter table location modify object_type not null;
-alter table location modify tsuid drop not null;
+alter table location modify tsuid null;
 create table uidmap (pk number(19,0) not null, uidmap blob not null, primary key (pk));
 create index UK_i1lnahmehau3r3j9pdyxg3p3y on location (multi_ref);
 alter table location add constraint FK_bfk5vl6eoxaf0hhwiu3rbgmkn foreign key (uidmap_fk) references uidmap;
-
---to be checked--
 create index FK_bfk5vl6eoxaf0hhwiu3rbgmkn on location (uidmap_fk) ;
---to be checked--
-
 create sequence uidmap_pk_seq;
