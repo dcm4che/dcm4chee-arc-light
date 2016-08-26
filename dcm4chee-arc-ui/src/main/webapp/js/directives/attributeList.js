51b4fdbde130957f0fd78bf5c00c8e13453311ba
Gunter Zeilinger <gunterze@gmail.com>
2016-08-26 13:24:45 +0200

    Fix dcm4che/dcm4chee-arc-light#329: UI : Show private attribute names of Patient/Study/Series/Instance #329

--- dcm4chee-arc-ui/src/main/webapp/js/directives/attributeList.js
+++ dcm4chee-arc-ui/src/main/webapp/js/directives/attributeList.js
@@ -2,8 +2,20 @@
 
 myApp.directive("attributeList", function() {
     function attrs2rows(level, attrs, rows) {
-        angular.forEach(attrs, function (el, tag) {
-            rows.push({ level: level, tag: tag, el: el });
+        function privateCreator(tag) {
+            if ("02468ACE".indexOf(tag.charAt(3)) < 0) {
+                var block = tag.slice(4, 6);
+                if (block !== "00") {
+                    var el = attrs[tag.slice(0, 4) + "00" + block];
+                    return el && el.Value && el.Value[0];
+                }
+            }
+            return undefined;
+        }
+
+        Object.keys(attrs).sort().forEach(function (tag) {
+            var el = attrs[tag];
+            rows.push({ level: level, tag: tag, name: DCM4CHE.elementName.forTag(tag, privateCreator(tag)), el: el });
             if (el.vr === 'SQ') {
                 var itemLevel = level + ">";
                 angular.forEach(el.Value, function (item, index) {
