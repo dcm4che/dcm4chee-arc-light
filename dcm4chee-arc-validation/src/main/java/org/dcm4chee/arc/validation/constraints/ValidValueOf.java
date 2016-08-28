8029328022409cf8800209d501fff112c411b572
Gunter Zeilinger <gunterze@gmail.com>
2016-08-28 08:51:10 +0200

    dcm4che/dcm4chee-arc-light#337: RESTful Service to update the expiration date of Study and Series

--- dcm4chee-arc-validation/src/main/java/org/dcm4chee/arc/validation/constraints/ValidValueOf.java
+++ dcm4chee-arc-validation/src/main/java/org/dcm4chee/arc/validation/constraints/ValidValueOf.java
@@ -44,7 +44,6 @@ import javax.validation.Constraint;
 import javax.validation.Payload;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
-import java.lang.reflect.Method;
 
 import static java.lang.annotation.ElementType.*;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
