bef3a51dcfebe7f4de6f9fffa313d31fbdf1082c
Gunter Zeilinger <gunterze@gmail.com>
2016-11-17 13:04:05 +0100

    Fix #455: Consider configured Masquarade Calling AETs also for forwarding C-Move RQs to Fallback C-MOVE-RSP

--- dcm4chee-arc-retrieve-scu/src/main/java/org/dcm4chee/arc/retrieve/scu/impl/CMoveSCUImpl.java
+++ dcm4chee-arc-retrieve-scu/src/main/java/org/dcm4chee/arc/retrieve/scu/impl/CMoveSCUImpl.java
@@ -136,7 +136,8 @@ public class CMoveSCUImpl implements CMoveSCU {
         PresentationContext rqpc = as.getAAssociateRQ().getPresentationContext(pc.getPCID());
         AAssociateRQ aarq = new AAssociateRQ();
         if (!otherCMoveSCP.equals(ctx.getArchiveAEExtension().alternativeCMoveSCP()))
-            aarq.setCallingAET(as.getCallingAET());
+            if (!ctx.getLocalApplicationEntity().isMasqueradeCallingAETitle(otherCMoveSCP))
+                aarq.setCallingAET(as.getCallingAET());
         aarq.addPresentationContext(rqpc);
         aarq.addExtendedNegotiation(new ExtendedNegotiation(rqpc.getAbstractSyntax(),
                 QueryOption.toExtendedNegotiationInformation(EnumSet.of(QueryOption.RELATIONAL))));
