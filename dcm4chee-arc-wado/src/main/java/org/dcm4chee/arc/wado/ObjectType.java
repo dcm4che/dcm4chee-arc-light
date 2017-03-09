cc29a8aebb8c389278dcf29ab9829977e0d25446
Gunter Zeilinger <gunterze@gmail.com>
2017-03-09 12:28:41 +0100

    WADO-URI: Retrieve Encapsulated PDF with contentType=application/dicom fails with HTTP error 406 - Not Acceptable Fix #574

--- dcm4chee-arc-wado/src/main/java/org/dcm4chee/arc/wado/ObjectType.java
+++ dcm4chee-arc-wado/src/main/java/org/dcm4chee/arc/wado/ObjectType.java
@@ -130,11 +130,11 @@ enum ObjectType {
             },
             null),
     EncapsulatedPDF(
-            new MediaType[] { MediaTypes.APPLICATION_PDF_TYPE },
-            new MediaType[] { MediaTypes.APPLICATION_DICOM_TYPE, MediaTypes.APPLICATION_PDF_TYPE }),
+            new MediaType[] { MediaTypes.APPLICATION_PDF_TYPE, MediaTypes.APPLICATION_DICOM_TYPE },
+            new MediaType[] { MediaTypes.APPLICATION_DICOM_TYPE }),
     EncapsulatedCDA(
-            new MediaType[] { MediaType.TEXT_XML_TYPE },
-            new MediaType[] { MediaTypes.APPLICATION_DICOM_TYPE, MediaType.TEXT_XML_TYPE }),
+            new MediaType[] { MediaType.TEXT_XML_TYPE, MediaTypes.APPLICATION_DICOM_TYPE },
+            new MediaType[] { MediaType.TEXT_XML_TYPE }),
     Other(new MediaType[] { MediaTypes.APPLICATION_DICOM_TYPE }, null);
 
     private final MediaType[] mimeTypes;
