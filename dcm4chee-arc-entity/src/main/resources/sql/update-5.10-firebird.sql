95aa87ed503c2e34e1ba1ccfd525f86509d4d944
vrindanayak <vrinda.nayak@j4care.com>
2017-03-28 17:51:33 +0200

    dcm4che/dcm4chee-arc-light#608 : Update db scripts for 5.10.0 for databases other than postgres

---
+++ dcm4chee-arc-entity/src/main/resources/sql/update-5.10-firebird.sql
@@ -0,0 +1,35 @@
+update study set ext_retrieve_aet = '*' where ext_retrieve_aet is null;
+alter table study add ext_retrieve_aet1 varchar(255) not null;
+update STUDY set EXT_RETRIEVE_AET1=EXT_RETRIEVE_AET;
+drop index UK_cl9dmi0kb97ov1cjh7rn3dhve;
+alter TABLE STUDY DROP ext_retrieve_aetNew;
+alter TABLE STUDY alter COLUMN EXT_RETRIEVE_AET1 to EXT_RETRIEVE_AET;
+create index UK_cl9dmi0kb97ov1cjh7rn3dhve on study (ext_retrieve_aet);
+
+
+update queue_msg set queue_name = '*' where queue_name is null;
+alter table queue_msg add queue_name1 varchar(255) not null;
+update queue_msg set queue_name1=queue_name;
+drop index UK_b5mbe6jenklf1r5wp5csrvf67;
+alter table QUEUE_MSG drop queue_name;
+alter table QUEUE_MSG alter COLUMN queue_name1 to queue_name;
+create index UK_b5mbe6jenklf1r5wp5csrvf67 on queue_msg (queue_name);
+
+
+alter table export_task add created_time timestamp not null;
+alter table export_task add updated_time timestamp not null;
+alter table export_task add num_instances integer;
+alter table export_task add modalities varchar(255);
+alter table export_task add queue_msg_fk numeric(18,0);
+update export_task set created_time = current_timestamp, updated_time = current_timestamp;
+
+
+alter table export_task drop constraint UK_aoqbyfnen6evu73ltc1osexfr;
+alter table export_task add constraint FK_g6atpiywpo2100kn6ovix7uet foreign key (queue_msg_fk) references queue_msg;
+
+drop index UK_cxaqwh62doxvy1itpdi43c681;
+create index UK_c5cof80jx0oopvovf3p4jv4l8 on export_task (device_name);
+create index UK_p5jjs08sdp9oecvr93r2g0kyq on export_task (updated_time);
+create index UK_j1t0mj3vlmf5xwt4fs5xida1r on export_task (scheduled_time);
+create index UK_q7gmfr3aog1hateydhfeiu7si on export_task (exporter_id);
+create index UK_hb9rftf7opmg56nkg7dkvsdc8 on export_task (study_iuid, series_iuid, sop_iuid);
---
+++ dcm4chee-arc-entity/src/main/resources/sql/update-5.10-h2.sql
@@ -0,0 +1,23 @@
+update study set ext_retrieve_aet = '*' where ext_retrieve_aet is null;
+alter table study alter ext_retrieve_aet set not null;
+
+alter table queue_msg alter queue_name set not null;
+alter table export_task add created_time timestamp;
+alter table export_task add updated_time timestamp;
+alter table export_task add num_instances int4;
+alter table export_task add modalities varchar(255);
+alter table export_task add queue_msg_fk int8;
+update export_task set created_time = current_timestamp, updated_time = current_timestamp;
+alter table export_task alter created_time set not null;
+alter table export_task alter updated_time set not null;
+
+alter table export_task drop constraint UK_aoqbyfnen6evu73ltc1osexfr;
+alter table export_task add constraint FK_g6atpiywpo2100kn6ovix7uet foreign key (queue_msg_fk) references queue_msg;
+
+drop index UK_cxaqwh62doxvy1itpdi43c681;
+create index UK_c5cof80jx0oopvovf3p4jv4l8 on export_task (device_name);
+create index UK_p5jjs08sdp9oecvr93r2g0kyq on export_task (updated_time);
+create index UK_j1t0mj3vlmf5xwt4fs5xida1r on export_task (scheduled_time);
+create index UK_q7gmfr3aog1hateydhfeiu7si on export_task (exporter_id);
+create index UK_hb9rftf7opmg56nkg7dkvsdc8 on export_task (study_iuid, series_iuid, sop_iuid);
+create index FK_g6atpiywpo2100kn6ovix7uet on export_task (queue_msg_fk);
