39ed279f4a2470d9ab61c41162e8bee8c7237bfe
Gunter Zeilinger <gunterze@gmail.com>
2017-05-17 09:00:59 +0200

    Remove legacy Angular JS 1.x based UI Fix #702

--- dcm4chee-arc-ui/src/main/webapp/js/directives/device_selectTransferCapabilityDir.js
+++
@@ -1,8 +0,0 @@
-// "use strict";
-
-// myApp.directive("selectTransfareCapability",function(){
-// 	return{
-// 		restrict: 'A',
-// 		templateUrl: 'templates/device_selectTransfareCap.html'
-// 	}
-// });
