288b1ac109c6951e759852737c05a2fd681f7dcf
Shefki Esadi <shefki_esadi@hotmail.de>
2017-06-27 16:19:00 +0200

    [UI: prevent tooltip directive to show/render tooltip if the text is shorter than 2 characters #824]

--- dcm4chee-arc-ui2/src/app/helpers/tooltip/tooltip.directive.ts
+++ dcm4chee-arc-ui2/src/app/helpers/tooltip/tooltip.directive.ts
@@ -3,7 +3,7 @@ import {Input, HostListener} from '@angular/core';
 import * as _ from 'lodash';
 
 @Directive({
-  selector: '[tooltip]'
+    selector: '[tooltip]'
 })
 export class TooltipDirective {
     @Input() tooltip: string;
@@ -19,66 +19,76 @@ export class TooltipDirective {
     @HostListener('mouseenter') onMouseEnter() {
         if (!this.placeholderSet){
             this.createPlaceholder();
+            this.showTooltip();
+        }else{
+            this.showTooltip();
         }
-        this.showTooltip();
     }
     @HostListener('mouseleave') onMouseLeave() {
-        this.hideTooltip();
+        if (this.placeholderSet){
+            this.hideTooltip();
+        }
     }
     @HostListener('onmouseup') onMouseUp() {
         window.prompt('Copy to clipboard: Ctrl+C, Enter', this.tooltip);
     }
     createPlaceholder(){
-        this.div =  document.createElement('div');
-        this.div.className = 'tooltip_container';
-        let div2 =  document.createElement('div');
-        div2.className = 'dir-tooltip animated';
-        let br = document.createElement('br');
+        if(this.tooltip && this.tooltip.length > 1){
+            this.div =  document.createElement('div');
+            this.div.className = 'tooltip_container';
+            let div2 =  document.createElement('div');
+            div2.className = 'dir-tooltip animated';
+            let br = document.createElement('br');
 
-        this.div.addEventListener('mouseup', () => {
-            window.prompt('Copy to clipboard: Ctrl+C, Enter', this.tooltip);
-        });
-        let text;
-        if (_.includes(this.tooltip, '<br>')){
-            let textArray = this.tooltip.split('<br>');
-            _.forEach(textArray, (m, i) => {
-                div2.appendChild(br);
-                div2.appendChild(document.createTextNode(m));
+            this.div.addEventListener('mouseup', () => {
+                window.prompt('Copy to clipboard: Ctrl+C, Enter', this.tooltip);
             });
-        }else{
-            text = document.createTextNode(this.tooltip);
-            div2.appendChild(text);
-        }
+            let text;
+            if (_.includes(this.tooltip, '<br>')){
+                let textArray = this.tooltip.split('<br>');
+                _.forEach(textArray, (m, i) => {
+                    div2.appendChild(br);
+                    div2.appendChild(document.createTextNode(m));
+                });
+            }else{
+                text = document.createTextNode(this.tooltip);
+                div2.appendChild(text);
+            }
 
-        this.div.appendChild(div2);
-        // this.el.nativeElement.addEventListener("mouseup",()=>{
-        //     window.prompt("Copy to clipboard: Ctrl+C, Enter4", this.tooltip);
-        // });
-        this.el.nativeElement.appendChild(this.div);
-        this.div.addEventListener('dblclick', () => {
-            window.prompt('Copy to clipboard: Ctrl+C, Enter', this.tooltip);
-        });
-        this.placeholderSet = true;
+            this.div.appendChild(div2);
+            // this.el.nativeElement.addEventListener("mouseup",()=>{
+            //     window.prompt("Copy to clipboard: Ctrl+C, Enter4", this.tooltip);
+            // });
+            this.el.nativeElement.appendChild(this.div);
+            this.div.addEventListener('dblclick', () => {
+                window.prompt('Copy to clipboard: Ctrl+C, Enter', this.tooltip);
+            });
+            this.placeholderSet = true;
+        }
     }
     showTooltip(){
-        this.div.classList.add('openflag');
-        this.div.classList.remove('closeflag');
-        setTimeout(() => {
-            if (this.div.classList.contains('openflag')){
-                this.div.classList.add('show');
-                this.div.classList.remove('closeflag');
-                this.div.classList.remove('openflag');
-            }
-        }, 800);
+        if(this.placeholderSet){
+            this.div.classList.add('openflag');
+            this.div.classList.remove('closeflag');
+            setTimeout(() => {
+                if (this.div.classList.contains('openflag')){
+                    this.div.classList.add('show');
+                    this.div.classList.remove('closeflag');
+                    this.div.classList.remove('openflag');
+                }
+            }, 800);
+        }
     };
     hideTooltip(){
-        this.div.classList.add('closeflag');
-        this.div.classList.remove('openflag');
-        setTimeout(() => {
-            if (this.div.classList.contains('closeflag')){
-                this.div.classList.remove('closeflag');
-                this.div.classList.remove('show');
-            }
-        }, 500);
+        if(this.placeholderSet){
+            this.div.classList.add('closeflag');
+            this.div.classList.remove('openflag');
+            setTimeout(() => {
+                if (this.div.classList.contains('closeflag')){
+                    this.div.classList.remove('closeflag');
+                    this.div.classList.remove('show');
+                }
+            }, 500);
+        }
     }
 }
