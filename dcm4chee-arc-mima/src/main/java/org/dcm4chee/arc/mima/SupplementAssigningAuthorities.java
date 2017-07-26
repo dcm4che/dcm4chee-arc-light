58f9c539e0dbb6766c3ea9f4caf7511c1dcf73d1
vrinda <vrinda.nayak@j4care.com>
2017-07-26 17:54:41 +0200

    dcm4che/dcm4chee-arc-light#888 : Supplement received Composite Objects with Issuer Of Patient ID and other attributes from configuration

--- dcm4chee-arc-mima/src/main/java/org/dcm4chee/arc/mima/SupplementAssigningAuthorities.java
+++ dcm4chee-arc-mima/src/main/java/org/dcm4chee/arc/mima/SupplementAssigningAuthorities.java
@@ -38,15 +38,19 @@
 
 package org.dcm4chee.arc.mima;
 
-import org.dcm4che3.data.Attributes;
-import org.dcm4che3.data.AttributesCoercion;
+import org.dcm4che3.data.*;
 import org.dcm4che3.net.Device;
+import org.dcm4che3.util.TagUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author Gunter Zeilinger <gunterze@gmail.com>
  * @since Jul 2017
  */
 public class SupplementAssigningAuthorities implements AttributesCoercion {
+    static final Logger LOG = LoggerFactory.getLogger(SupplementAssigningAuthorities.class);
+    private static final ElementDictionary DICT = ElementDictionary.getStandardElementDictionary();
 
     private final Entity entity;
     private final Device device;
@@ -92,11 +96,134 @@ public class SupplementAssigningAuthorities implements AttributesCoercion {
     }
 
     private void supplementInstance(Attributes attrs) {
-        //TODO
+        supplementValue(attrs, Tag.Manufacturer, VR.LO, device.getManufacturer());
+        supplementValue(attrs, Tag.ManufacturerModelName, VR.LO, device.getManufacturerModelName());
+        supplementValue(attrs, Tag.StationName, VR.SH, device.getStationName());
+        supplementValue(attrs, Tag.DeviceSerialNumber, VR.LO, device.getDeviceSerialNumber());
+        supplementValues(attrs, Tag.SoftwareVersions, VR.LO, device.getSoftwareVersions());
+        supplementValue(attrs, Tag.InstitutionName, VR.LO, device.getInstitutionNames());
+        supplementCode(attrs, Tag.InstitutionCodeSequence, device.getInstitutionCodes());
+        supplementValue(attrs, Tag.InstitutionalDepartmentName, VR.LO, device.getInstitutionalDepartmentNames());
+        supplementIssuers(attrs);
+        supplementRequestIssuers(attrs);
+        supplementRequestIssuers(attrs.getSequence(Tag.RequestAttributesSequence));
     }
 
     private void supplementMPPS(Attributes attrs) {
         //TODO
     }
 
+    private void supplementValue(Attributes attrs, int tag, VR vr, String... values) {
+        if (values.length == 0 || values[0] == null || attrs.containsValue(tag))
+            return;
+
+        attrs.setString(tag, vr, values[0]);
+        log(tag, vr, values[0]);
+    }
+
+    private void supplementValues(Attributes attrs, int tag, VR vr, String... values) {
+        if (values.length == 0 || attrs.containsValue(tag))
+            return;
+
+        attrs.setString(tag, vr, values);
+        log(tag, vr, values);
+    }
+
+
+    private void supplementCode(Attributes attrs, int seqTag, Code... codes) {
+        if (codes.length == 0 || codes[0] == null || attrs.containsValue(seqTag))
+            return;
+
+        Attributes item = new Attributes(attrs.bigEndian(), 4);
+        item.setString(Tag.CodeValue, VR.SH, codes[0].getCodeValue());
+        item.setString(Tag.CodingSchemeDesignator, VR.SH, codes[0].getCodingSchemeDesignator());
+        String version = codes[0].getCodingSchemeVersion();
+        if (version != null)
+            item.setString(Tag.CodingSchemeVersion, VR.SH, version);
+        item.setString(Tag.CodeMeaning, VR.LO, codes[0].getCodeMeaning());
+        attrs.newSequence(seqTag, 1).add(item);
+        log(seqTag, VR.SQ, item);
+    }
+
+    private void supplementIssuers(Attributes attrs) {
+        supplementIssuerOfPatientID(attrs);
+        if (attrs.containsValue(Tag.AdmissionID))
+            supplementIssuer(attrs, Tag.IssuerOfAdmissionIDSequence, device.getIssuerOfAdmissionID());
+        if (attrs.containsValue(Tag.ServiceEpisodeID))
+            supplementIssuer(attrs, Tag.IssuerOfServiceEpisodeID, device.getIssuerOfServiceEpisodeID());
+        if (attrs.containsValue(Tag.ContainerIdentifier))
+            supplementIssuer(attrs, Tag.IssuerOfTheContainerIdentifierSequence, device.getIssuerOfContainerIdentifier());
+        if (attrs.containsValue(Tag.SpecimenIdentifier))
+            supplementIssuer(attrs, Tag.IssuerOfTheSpecimenIdentifierSequence, device.getIssuerOfSpecimenIdentifier());
+    }
+
+    private void supplementIssuerOfPatientID(Attributes attrs) {
+        if (supplementIssuerOfPID(attrs)) {
+            return;
+        }
+
+        Issuer issuer = device.getIssuerOfPatientID();
+        String localNamespaceEntityID = issuer.getLocalNamespaceEntityID();
+        if (localNamespaceEntityID != null) {
+            attrs.setString(Tag.IssuerOfPatientID, VR.LO, localNamespaceEntityID);
+            log(Tag.IssuerOfPatientID, VR.LO, localNamespaceEntityID);
+        }
+        String universalEntityID = issuer.getUniversalEntityID();
+        if (universalEntityID != null) {
+            Attributes item = new Attributes(attrs.bigEndian(), 2);
+            item.setString(Tag.UniversalEntityID, VR.UT, universalEntityID);
+            item.setString(Tag.UniversalEntityIDType, VR.CS,
+                    issuer.getUniversalEntityIDType());
+            attrs.newSequence(Tag.IssuerOfPatientIDQualifiersSequence, 1).add(item);
+            log(Tag.IssuerOfPatientIDQualifiersSequence, VR.SQ, item);
+        }
+    }
+
+    private boolean supplementIssuerOfPID(Attributes attrs) {
+        return !attrs.containsValue(Tag.PatientID)
+                || device.getIssuerOfPatientID() == null
+                || attrs.containsValue(Tag.IssuerOfPatientID)
+                || attrs.containsValue(Tag.IssuerOfPatientIDQualifiersSequence);
+    }
+
+
+    private void supplementIssuer(Attributes attrs, int seqTag, Issuer issuer) {
+        if (issuer == null || attrs.containsValue(seqTag))
+            return;
+
+        Attributes item = new Attributes(attrs.bigEndian(), 3);
+        String localNamespaceEntityID = issuer.getLocalNamespaceEntityID();
+        if (localNamespaceEntityID != null)
+            item.setString(Tag.LocalNamespaceEntityID, VR.LO, localNamespaceEntityID);
+        String universalEntityID = issuer.getUniversalEntityID();
+        if (universalEntityID != null) {
+            item.setString(Tag.UniversalEntityID, VR.UT, universalEntityID);
+            item.setString(Tag.UniversalEntityIDType, VR.CS,
+                    issuer.getUniversalEntityIDType());
+        }
+        attrs.newSequence(seqTag, 1).add(item);
+        log(seqTag, VR.SQ, item);
+    }
+
+    private void supplementRequestIssuers(Sequence rqSeq) {
+        if (rqSeq != null)
+            for (Attributes rq : rqSeq)
+                supplementRequestIssuers(rq);
+    }
+
+    private void supplementRequestIssuers(Attributes rq) {
+        if (rq.containsValue(Tag.AccessionNumber))
+            supplementIssuer(rq, Tag.IssuerOfAccessionNumberSequence, device.getIssuerOfAccessionNumber());
+        if (rq.containsValue(Tag.PlacerOrderNumberImagingServiceRequest))
+            supplementIssuer( rq, Tag.OrderPlacerIdentifierSequence, device.getOrderPlacerIdentifier());
+        if (rq.containsValue(Tag.FillerOrderNumberImagingServiceRequest))
+            supplementIssuer(rq, Tag.OrderFillerIdentifierSequence, device.getOrderFillerIdentifier());
+    }
+
+    private void log(int tag, VR vr, Object value) {
+        if (LOG.isDebugEnabled())
+            LOG.debug("{}: Supplements {} {} [{}] {}",
+                    TagUtils.toString(tag), vr, value,
+                    DICT.keywordOf(tag));
+    }
 }
