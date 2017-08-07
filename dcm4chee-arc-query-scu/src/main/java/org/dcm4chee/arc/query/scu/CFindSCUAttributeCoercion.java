ef573be10aaf0923eb06bd68756c08843f200ede
Gunter Zeilinger <gunterze@gmail.com>
2017-08-07 11:56:17 +0200

    Coercion of Composite Query C-FIND RSP by XSLT or Leading C-FIND SCP #900

--- dcm4chee-arc-query-scu/src/main/java/org/dcm4chee/arc/query/scu/CFindSCUAttributeCoercion.java
+++ dcm4chee-arc-query-scu/src/main/java/org/dcm4chee/arc/query/scu/CFindSCUAttributeCoercion.java
@@ -47,6 +47,8 @@ import org.dcm4che3.net.ApplicationEntity;
 import org.dcm4che3.net.Priority;
 import org.dcm4che3.net.service.QueryRetrieveLevel2;
 import org.dcm4chee.arc.Cache;
+import org.dcm4chee.arc.LeadingCFindSCPQueryCache;
+import org.dcm4chee.arc.conf.ArchiveDeviceExtension;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -61,20 +63,19 @@ public class CFindSCUAttributeCoercion implements AttributesCoercion {
     private final static Logger LOG = LoggerFactory.getLogger(CFindSCUAttributeCoercion.class);
 
     private final ApplicationEntity localAE;
-    private final String calledAET;
-    private final int[] returnKeys;
+    private final String leadingCFindSCP;
     private final Attributes.UpdatePolicy attributeUpdatePolicy;
     private final CFindSCU cfindSCU;
-    private final Cache<String,Attributes> queryCache;
+    private final LeadingCFindSCPQueryCache queryCache;
     private final AttributesCoercion next;
 
     public CFindSCUAttributeCoercion(
-            ApplicationEntity localAE, String calledAET, int[] returnKeys,
-            Attributes.UpdatePolicy attributeUpdatePolicy, CFindSCU cfindSCU, Cache<String, Attributes> queryCache,
+            ApplicationEntity localAE, String leadingCFindSCP,
+            Attributes.UpdatePolicy attributeUpdatePolicy, CFindSCU cfindSCU,
+            LeadingCFindSCPQueryCache queryCache,
             AttributesCoercion next) {
         this.localAE = localAE;
-        this.calledAET = calledAET;
-        this.returnKeys = returnKeys;
+        this.leadingCFindSCP = leadingCFindSCP;
         this.attributeUpdatePolicy = attributeUpdatePolicy;
         this.cfindSCU = cfindSCU;
         this.queryCache = queryCache;
@@ -88,25 +89,28 @@ public class CFindSCUAttributeCoercion implements AttributesCoercion {
         if (newAttrs != null)
             attrs.update(attributeUpdatePolicy, newAttrs, modified);
         else
-            LOG.warn("Failed to query Study[{}] from {} - do not coerce attributes", studyIUID, calledAET);
+            LOG.warn("Failed to query Study[{}] from {} - do not coerce attributes", studyIUID, leadingCFindSCP);
         if (next != null)
             next.coerce(attrs, modified);
     }
 
     private Attributes queryStudy(String studyIUID) {
-        Cache.Entry<Attributes> entry = queryCache.getEntry(studyIUID);
+        LeadingCFindSCPQueryCache.Key key = new LeadingCFindSCPQueryCache.Key(leadingCFindSCP, studyIUID);
+        Cache.Entry<Attributes> entry = queryCache.getEntry(key);
         if (entry != null)
             return entry.value();
 
         Attributes newAttrs = null;
         try {
-            List<Attributes> matches = cfindSCU.find(localAE, calledAET, Priority.NORMAL,
-                    QueryRetrieveLevel2.STUDY, studyIUID, null, null, returnKeys);
+            ArchiveDeviceExtension arcdev = localAE.getDevice().getDeviceExtensionNotNull(ArchiveDeviceExtension.class);
+            List<Attributes> matches = cfindSCU.find(localAE, leadingCFindSCP, Priority.NORMAL,
+                    QueryRetrieveLevel2.STUDY, studyIUID, null, null,
+                    arcdev.returnKeysForLeadingCFindSCP(leadingCFindSCP));
             if (!matches.isEmpty())
                 newAttrs = matches.get(0);
         } catch (Exception e) {
         }
-        queryCache.put(studyIUID, newAttrs);
+        queryCache.put(key, newAttrs);
         return newAttrs;
     }
 }
