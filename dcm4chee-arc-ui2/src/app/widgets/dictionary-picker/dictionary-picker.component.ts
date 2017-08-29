5d6546b0a9abbdd91566f544b958ca13359a18c3
Shefki Esadi <shefki_esadi@hotmail.de>
2017-08-29 15:06:40 +0200

    adding dictionary-picker for SopClass, TransfarSyntace and Tags[UI Configuration: Prevent Users to insert invalid data #751]

---
+++ dcm4chee-arc-ui2/src/app/widgets/dictionary-picker/dictionary-picker.component.ts
@@ -0,0 +1,67 @@
+import {
+    Component, OnInit, EventEmitter, Input, Output
+} from '@angular/core';
+declare var DCM4CHE: any;
+import * as _ from 'lodash';
+import {SearchPipe} from "../../pipes/search.pipe";
+
+@Component({
+  selector: 'dictionary-picker',
+  templateUrl: './dictionary-picker.component.html',
+  styleUrls: ['./dictionary-picker.component.css']
+})
+export class DictionaryPickerComponent implements OnInit {
+    Object = Object;
+    @Input() dictionary;
+    @Input() formelement;
+    @Output() onValueSet = new EventEmitter();
+    filter = '';
+    dcmTags = [];
+    constructor(
+    ) { }
+
+    ngOnInit() {
+        switch(this.dictionary) {
+            case 'dcmTag':
+                _.forEach(DCM4CHE.elementName.forTag("all"),(m,i)=>{
+                    this.dcmTags.push({
+                        key:i,
+                        text:m
+                    })
+                });
+                break;
+            case 'dcmTransferSyntax':
+                _.forEach(DCM4CHE.TransferSyntax.nameOf("all"),(m,i)=>{
+                    this.dcmTags.push({
+                        key:i,
+                        text:m
+                    })
+                });
+                break;
+            case 'dcmSOPClass':
+                _.forEach(DCM4CHE.SOPClass.nameOf("all"),(m,i)=>{
+                    this.dcmTags.push({
+                        key:i,
+                        text:m
+                    })
+                });
+                break;
+        }
+    }
+    ngAfterViewInit() {
+        $('.dictionary_widget_search').focus();
+    }
+    addSelectedElement(element){
+        this.onValueSet.emit(element.key);
+    }
+
+    keyDown(e){
+        if(e.keyCode === 13){
+            let filtered = new SearchPipe().transform(this.dcmTags, this.filter);
+            if(filtered.length > 0){
+                this.onValueSet.emit(filtered[0].key);
+            }
+        }
+    }
+
+}
