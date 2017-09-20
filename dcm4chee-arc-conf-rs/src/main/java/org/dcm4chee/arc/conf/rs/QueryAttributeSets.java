e82b14f78442bb707fb2233ba21e24281e784dcd
Gunter Zeilinger <gunterze@gmail.com>
2017-09-20 17:35:34 +0200

    RESTful Service /attribute-set/{type} return 404 instead 500 for undefined type fix #968

--- dcm4chee-arc-conf-rs/src/main/java/org/dcm4chee/arc/conf/rs/QueryAttributeSets.java
+++ dcm4chee-arc-conf-rs/src/main/java/org/dcm4chee/arc/conf/rs/QueryAttributeSets.java
@@ -49,10 +49,8 @@ import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.json.Json;
 import javax.json.stream.JsonGenerator;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.core.StreamingOutput;
 import java.io.IOException;
 import java.io.OutputStream;
@@ -80,11 +78,11 @@ public class QueryAttributeSets {
     @Path("/{type}")
     @Produces("application/json")
     public StreamingOutput listAttributeSets(@PathParam("type") String type) {
+        final AttributeSet.Type attrSetType = attrSetTypeOf(type);
         return new StreamingOutput() {
             @Override
             public void write(OutputStream out) throws IOException {
                 JsonGenerator gen = Json.createGenerator(out);
-                AttributeSet.Type attrSetType = AttributeSet.Type.valueOf(type);
                 ArchiveDeviceExtension arcDev = device.getDeviceExtensionNotNull(ArchiveDeviceExtension.class);
                 gen.writeStartArray();
                 for (AttributeSet attrSet : toInstalledSortedAttrSet(arcDev.getAttributeSet(attrSetType))) {
@@ -103,6 +101,14 @@ public class QueryAttributeSets {
                 gen.flush();
             }
         };
+    }
+
+    private AttributeSet.Type attrSetTypeOf(String type) {
+        try {
+            return AttributeSet.Type.valueOf(type);
+        } finally {
+            throw new WebApplicationException(Response.Status.NOT_FOUND);
+        }
     }
 
     private List<AttributeSet> toInstalledSortedAttrSet(Map<String, AttributeSet> attrSets) {
