8f7fa9d790e23baad8c0ff305bf2413ba5469db8
vrinda <vrinda.nayak@j4care.com>
2017-10-10 17:18:27 +0200

    Fix #1009 dcm4che/dcm4chee-arc-light : IOCM-RS Reject Study/Series/Instance error case - Error message not sent back in response

--- dcm4chee-arc-iocm-rs/src/main/java/org/dcm4chee/arc/iocm/rs/IocmRS.java
+++ dcm4chee-arc-iocm-rs/src/main/java/org/dcm4chee/arc/iocm/rs/IocmRS.java
@@ -46,6 +46,7 @@ import org.dcm4che3.json.JSONReader;
 import org.dcm4che3.json.JSONWriter;
 import org.dcm4che3.net.ApplicationEntity;
 import org.dcm4che3.net.Device;
+import org.dcm4che3.net.service.DicomServiceException;
 import org.dcm4che3.util.UIDUtils;
 import org.dcm4chee.arc.entity.*;
 import org.dcm4chee.arc.hl7.RESTfulHL7Sender;
@@ -620,24 +621,30 @@ public class IocmRS {
     }
 
     private void reject(RSOperation rsOp, String studyUID, String seriesUID, String objectUID,
-                        String codeValue, String designator) throws IOException {
+                        String codeValue, String designator) throws Exception {
         logRequest();
-        ArchiveAEExtension arcAE = getArchiveAE();
-        RejectionNote rjNote = toRejectionNote(arcAE, codeValue, designator);
-        StoreSession session = storeService.newStoreSession(request, aet, arcAE.getApplicationEntity());
-        storeService.restoreInstances(session, studyUID, seriesUID);
+        try {
+            ArchiveAEExtension arcAE = getArchiveAE();
+            RejectionNote rjNote = toRejectionNote(arcAE, codeValue, designator);
+            StoreSession session = storeService.newStoreSession(request, aet, arcAE.getApplicationEntity());
+            storeService.restoreInstances(session, studyUID, seriesUID);
 
-        Attributes attrs = queryService.createRejectionNote(
-                arcAE.getApplicationEntity(), studyUID, seriesUID, objectUID, rjNote);
-        if (attrs == null)
-            throw new WebApplicationException(getResponse("No Study with UID: " + studyUID, Response.Status.NOT_FOUND));
+            Attributes attrs = queryService.createRejectionNote(
+                    arcAE.getApplicationEntity(), studyUID, seriesUID, objectUID, rjNote);
+            if (attrs == null)
+                throw new WebApplicationException(getResponse("No Study with UID: " + studyUID, Response.Status.NOT_FOUND));
 
-        StoreContext ctx = storeService.newStoreContext(session);
-        ctx.setSopClassUID(attrs.getString(Tag.SOPClassUID));
-        ctx.setSopInstanceUID(attrs.getString(Tag.SOPInstanceUID));
-        ctx.setReceiveTransferSyntax(UID.ExplicitVRLittleEndian);
-        storeService.store(ctx, attrs);
-        rsForward.forward(rsOp, arcAE, null, request);
+            StoreContext ctx = storeService.newStoreContext(session);
+            ctx.setSopClassUID(attrs.getString(Tag.SOPClassUID));
+            ctx.setSopInstanceUID(attrs.getString(Tag.SOPInstanceUID));
+            ctx.setReceiveTransferSyntax(UID.ExplicitVRLittleEndian);
+            storeService.store(ctx, attrs);
+            rsForward.forward(rsOp, arcAE, null, request);
+        } catch (DicomServiceException e) {
+            throw new WebApplicationException(getResponse(e.getStatus() + " : " + e.getMessage(), Response.Status.BAD_REQUEST));
+        } catch (Exception e) {
+            throw new WebApplicationException(getResponse(e.getMessage(), Response.Status.INTERNAL_SERVER_ERROR));
+        }
     }
 
     private Response copyOrMoveInstances(
