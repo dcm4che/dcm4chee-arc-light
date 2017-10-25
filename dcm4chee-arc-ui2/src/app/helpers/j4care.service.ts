3278ef46e2ae62e5837e7aaf583682e44edb3794
shefki <shralsheki@gmail.com>
2017-10-25 15:44:41 +0200

    experimenting with get download file[UI: Downloading files (Vendor, DICOM-file etc) in an secured api, don't work #1038]

--- dcm4chee-arc-ui2/src/app/helpers/j4care.service.ts
+++ dcm4chee-arc-ui2/src/app/helpers/j4care.service.ts
@@ -1,14 +1,17 @@
 import { Injectable } from '@angular/core';
 import {AppService} from "../app.service";
 import {J4careHttpService} from "./j4care-http.service";
+import {Http, ResponseContentType, Headers} from "@angular/http";
+import {Subscriber} from "rxjs/Subscriber";
+import {Observable} from "rxjs/Observable";
 declare var fetch;
-declare var Headers;
 @Injectable()
 export class j4care {
-
+    header = new Headers();
     constructor(
         public mainservice:AppService,
-        public httpJ4car:J4careHttpService
+        public httpJ4car:J4careHttpService,
+        public ngHttp:Http
     ) {}
     static traverse(object,func){
         for(let key in object){
@@ -26,6 +29,8 @@ export class j4care {
     download(url){
         this.httpJ4car.refreshToken().subscribe((res)=>{
             let token;
+            let a = document.createElement('a');
+
             if(res.length && res.length > 0){
                 this.httpJ4car.resetAuthenticationInfo(res);
                 token = res.token;
@@ -33,10 +38,12 @@ export class j4care {
                 token = this.mainservice.global.authentication.token;
             }
 /*            let xhttp = new XMLHttpRequest();
-            let filename = url.substring(url.lastIndexOf("/") + 1).split("?")[0];
-            xhttp.onload = function() {
+            let filename = url.substring(url.lastIndexOf("/") + 1).split("?")[0];*/
+/*            xhttp.onload = function() {
                 let a = document.createElement('a');
                 a.href = window.URL.createObjectURL(xhttp.response); // xhr.response is a blob
+                xhttp.onreadystatechange = null;
+                xhttp.abort();
                 let attr = document.createAttribute("download");
                 // attr.value = "true";
                 a.setAttributeNode(attr);
@@ -45,12 +52,64 @@ export class j4care {
                 document.body.appendChild(a);
                 a.click();
                 a.remove();
+            };*/
+/*            xhttp.onreadystatechange = ()=>{
+                if (xhttp.readyState == 4){
+                    if ((xhttp.status == 200) || (xhttp.status == 0)){
+                        let a = document.createElement('a');
+                        a.href = window.URL.createObjectURL(xhttp.response); // xhr.response is a blob
+                        let attr = document.createAttribute("download");
+                        xhttp.abort();
+                        // attr.value = "true";
+                        a.setAttributeNode(attr);
+                        // a.download = filename; // Set the file name.
+                        a.style.display = 'none';
+                        document.body.appendChild(a);
+                        a.click();
+                        a.remove();
+
+                    }
+                }
             };
             xhttp.open("GET", url);
             xhttp.responseType = "blob";
             xhttp.setRequestHeader('Authorization', `Bearer ${token}`);
             xhttp.send();*/
-            console.log("token",token);
+            this.header.append('Authorization', `Bearer ${token}`);
+/*            this.header.append('Authorization', `Bearer ${token}`);
+            this.get(url).subscribe((res)=>{
+                a.href = res;
+                let attr = document.createAttribute("download");
+                a.setAttributeNode(attr);
+                // a.download = filename; // Set the file name.
+                a.style.display = 'none';
+                document.body.appendChild(a);
+                a.click();
+                a.remove();
+            },(err)=>{
+
+            })*/
+            console.log("header",this.header);
+            this.ngHttp.get(url,{
+                        headers:this.header,
+                        responseType: ResponseContentType.Blob
+                    })
+                .map(res => {
+                        return new Blob([res['_body']], {
+                            type: res.headers.get("Content-Type")
+                        });
+                })
+                .subscribe((myBlob)=> {
+                a.href = window.URL.createObjectURL(myBlob);
+                let attr = document.createAttribute("download");
+                a.setAttributeNode(attr);
+                // a.download = filename; // Set the file name.
+                a.style.display = 'none';
+                document.body.appendChild(a);
+                a.click();
+                a.remove();
+            });
+/*            console.log("token",token);
             var myHeaders = new Headers();
             myHeaders.append('Authorization', `Bearer ${token}`);
 
@@ -63,7 +122,7 @@ export class j4care {
             fetch(url,myInit)
                 .then((response)=> {
                     console.log("response.url",response);
-                    return response.body.getReader();
+                    return response.blob();
                 })
                 .then((myBlob)=> {
                     a.href = window.URL.createObjectURL(myBlob);
@@ -74,7 +133,29 @@ export class j4care {
                     document.body.appendChild(a);
                     a.click();
                     a.remove();
+                });*/
+        });
+    }
+    get(url: string): Observable<any> {
+        return new Observable((observer: Subscriber<any>) => {
+            let objectUrl: string = null;
+
+            this.ngHttp
+                .get(url, {
+                    headers:this.header,
+                    responseType: ResponseContentType.Blob
+                })
+                .subscribe(m => {
+                    objectUrl = URL.createObjectURL(m.blob());
+                    observer.next(objectUrl);
                 });
+
+            return () => {
+                if (objectUrl) {
+                    URL.revokeObjectURL(objectUrl);
+                    objectUrl = null;
+                }
+            };
         });
     }
 }
