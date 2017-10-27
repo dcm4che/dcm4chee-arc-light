9e6787f2f4e305a62fafbad438edcbf2024e314f
Gunter Zeilinger <gunterze@gmail.com>
2017-10-27 17:43:32 +0200

    Fix #1039: QIDO-RS fails with 400 Bad Request, if OAuth2 bearer token is passed by Query Parameter access_token

--- dcm4chee-arc-query-util/src/main/java/org/dcm4chee/arc/query/util/QueryAttributes.java
+++ dcm4chee-arc-query-util/src/main/java/org/dcm4chee/arc/query/util/QueryAttributes.java
@@ -75,6 +75,7 @@ public class QueryAttributes {
                 case "orderby":
                     addOrderByTag(entry.getValue());
                     break;
+                case "access_token":
                 case "comparefield":
                 case "count":
                 case "different":
