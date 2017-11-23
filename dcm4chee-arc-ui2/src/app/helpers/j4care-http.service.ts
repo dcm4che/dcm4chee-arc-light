cf20381d8282378a1be9488ebe4a81e0fc1619f3
shefki <shralsheki@gmail.com>
2017-11-23 10:09:09 +0100

    UI: TypeError: Cannot set property 'getRealmStateActive' of undefined #1079

--- dcm4chee-arc-ui2/src/app/helpers/j4care-http.service.ts
+++ dcm4chee-arc-ui2/src/app/helpers/j4care-http.service.ts
@@ -35,12 +35,13 @@ export class J4careHttpService{
         let headerIndex = (param.length === 3) ? 2:1;
         $this.setHeader(param[headerIndex]);
         return $this.refreshToken().flatMap((response)=>{
-                $this.mainservice.global.getRealmStateActive = false;
+                this.setValueInGlobal('getRealmStateActive',false);
                 if(response && response.length != 0){
                     if(response['token'] === null){
-                        $this.mainservice.global.notSecure = true;
+                        $this.setValueInGlobal('notSecure',true);
+
                     }else{
-                        $this.mainservice.global.notSecure = false;
+                        $this.setValueInGlobal('notSecure',false);
                         $this.resetAuthenticationInfo(response);
                         $this.token = response['token'];
                         // $this.setHeader(param[headerIndex]);
@@ -60,27 +61,16 @@ export class J4careHttpService{
         });
     }
     resetAuthenticationInfo(response){
-        let $this = this;
         let browserTime = Math.floor(Date.now() / 1000);
         if(response.systemCurrentTime != browserTime){
             let diffTime = browserTime - response.systemCurrentTime;
             response.expiration = response.expiration + diffTime;
         }
-        if ($this.mainservice.global && !$this.mainservice.global.authentication){
-            let global = _.cloneDeep($this.mainservice.global);
-            global.authentication = response;
-            $this.mainservice.setGlobal(global);
-        }else{
-            if ($this.mainservice.global && $this.mainservice.global.authentication){
-                $this.mainservice.global.authentication = response;
-            }else{
-                $this.mainservice.setGlobal({authentication: response});
-            }
-        }
+        this.setValueInGlobal('authentication',response);
     }
     refreshToken():Observable<any>{
         if((!_.hasIn(this.mainservice,"global.authentication") || !this.tokenValid()) && (!this.mainservice.global || !this.mainservice.global.notSecure) && (!this.mainservice.global || !this.mainservice.global.getRealmStateActive)){
-            this.mainservice.global.getRealmStateActive = true;
+            this.setValueInGlobal('getRealmStateActive',true);
             return this.$http.get('rs/realm').map(res => {
                 let resjson; try{ let pattern = new RegExp("[^:]*:\/\/[^\/]*\/auth\/");
                 if(pattern.exec(res.url)){
@@ -106,6 +96,21 @@ export class J4careHttpService{
         }
     }
 
+    setValueInGlobal(key, value){
+        if (this.mainservice.global && !this.mainservice.global[key]){
+            let global = _.cloneDeep(this.mainservice.global);
+            global[key] = value;
+            this.mainservice.setGlobal(global);
+        }else{
+            if (this.mainservice.global && this.mainservice.global[key]){
+                this.mainservice.global[key] = value;
+            }else{
+                let global = {};
+                global[key] = value;
+                this.mainservice.setGlobal(global);
+            }
+        }
+    }
     setHeader(header){
         if(header){
             if(this.token){
