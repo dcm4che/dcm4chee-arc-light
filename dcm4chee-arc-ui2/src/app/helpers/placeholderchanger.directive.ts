9bf88f9d90fef0ec1d2cfe1146210808d8c0eb38
shefki <shralsheki@gmail.com>
2017-12-12 13:33:58 +0100

    prepare for upgrade[UI: upgrade to angular 5 #1124]

--- dcm4chee-arc-ui2/src/app/helpers/placeholderchanger.directive.ts
+++ dcm4chee-arc-ui2/src/app/helpers/placeholderchanger.directive.ts
@@ -1,4 +1,4 @@
-import {Directive, ElementRef, OnInit, Renderer} from '@angular/core';
+import {Directive, ElementRef, OnInit, Renderer2} from '@angular/core';
 import {Input} from '@angular/core';
 import {Globalvar} from '../constants/globalvar';
 import * as _ from 'lodash';
@@ -10,16 +10,16 @@ export class PlaceholderchangerDirective implements OnInit{
 
     @Input('placeholderchanger') inputAttribut: any;
 
-    constructor(private el: ElementRef, private renderer: Renderer) {}
+    constructor(private el: ElementRef, private renderer: Renderer2) {}
 
     ngOnInit() {
         if(this.inputAttribut.code == '00100020' && this.inputAttribut.externalInternalAetMode == "external"){
-            this.renderer.setElementAttribute(this.el.nativeElement, 'placeholder', this.inputAttribut.name);
+            this.renderer.setAttribute(this.el.nativeElement, 'placeholder', this.inputAttribut.name);
         }else{
             if ((this.inputAttribut.code in Globalvar.IODPLACEHOLDERS) && _.hasIn(Globalvar.IODPLACEHOLDERS[this.inputAttribut.code], this.inputAttribut.mode)){
                 if (Globalvar.IODPLACEHOLDERS[this.inputAttribut.code][this.inputAttribut.mode].action === 'replace' && this.el.nativeElement.tagName === 'INPUT'){
-                    this.renderer.setElementAttribute(this.el.nativeElement, 'placeholder', Globalvar.IODPLACEHOLDERS[this.inputAttribut.code][this.inputAttribut.mode].placeholder);
-                    this.renderer.setElementAttribute(this.el.nativeElement, 'title', Globalvar.IODPLACEHOLDERS[this.inputAttribut.code][this.inputAttribut.mode].placeholder);
+                    this.renderer.setAttribute(this.el.nativeElement, 'placeholder', Globalvar.IODPLACEHOLDERS[this.inputAttribut.code][this.inputAttribut.mode].placeholder);
+                    this.renderer.setAttribute(this.el.nativeElement, 'title', Globalvar.IODPLACEHOLDERS[this.inputAttribut.code][this.inputAttribut.mode].placeholder);
                 }
                 if (Globalvar.IODPLACEHOLDERS[this.inputAttribut.code][this.inputAttribut.mode].action === 'disable'){
                     this.disableElement();
@@ -28,14 +28,14 @@ export class PlaceholderchangerDirective implements OnInit{
                 if (this.inputAttribut.iod && !_.hasIn(this.inputAttribut.iod, this.inputAttribut.code)){
                     this.disableElement();
                 }
-                this.renderer.setElementAttribute(this.el.nativeElement, 'placeholder', this.inputAttribut.name);
+                this.renderer.setAttribute(this.el.nativeElement, 'placeholder', this.inputAttribut.name);
             }
         }
 
     }
     disableElement(){
         if (this.el.nativeElement.tagName === 'INPUT'){
-            this.renderer.setElementProperty(this.el.nativeElement, 'disabled', true);
+            this.renderer.setProperty(this.el.nativeElement, 'disabled', true);
         }
         if (this.el.nativeElement.tagName === 'DIV'){
             this.el.nativeElement.style.display = 'none';
