dded4f2f3be42c5439827faa17b99a2853ea086d
Gunter Zeilinger <gunterze@gmail.com>
2018-02-19 16:49:24 +0100

    RESTful service to query Estonian National Patient Registry

---
+++ dcm4chee-arc-xroad-rs/src/main/java/org/dcm4chee/arc/xroad/rs/XRoadRS.java
@@ -0,0 +1,101 @@
+/*
+ * **** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is part of dcm4che, an implementation of DICOM(TM) in
+ * Java(TM), hosted at https://github.com/dcm4che.
+ *
+ * The Initial Developer of the Original Code is
+ * J4Care.
+ * Portions created by the Initial Developer are Copyright (C) 2015-2018
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ * See @authors listed below
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * **** END LICENSE BLOCK *****
+ *
+ */
+
+package org.dcm4chee.arc.xroad.rs;
+
+import org.dcm4che3.data.Attributes;
+import org.dcm4che3.json.JSONWriter;
+import org.dcm4chee.arc.xroad.XRoadException;
+import org.dcm4chee.arc.xroad.XRoadServiceProvider;
+import org.jboss.resteasy.annotations.cache.NoCache;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.json.Json;
+import javax.json.stream.JsonGenerator;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.StreamingOutput;
+
+/**
+ * @author Gunter Zeilinger <gunterze@gmail.com>
+ * @since Feb 2018
+ */
+@RequestScoped
+@Path("/xroad")
+public class XRoadRS {
+
+    @Inject
+    private XRoadServiceProvider service;
+
+    @GET
+    @NoCache
+    @Path("/RR441/{PatientID}")
+    @Produces("application/dicom+json,application/json")
+    public Response rr441(@PathParam("PatientID") String patientID) throws Exception {
+        Attributes attrs;
+        try {
+            attrs = service.rr441(patientID);
+        } catch (XRoadException e) {
+            return errResponse(e.getMessage(), Response.Status.BAD_GATEWAY);
+        }
+        return (attrs == null ? Response.status(Response.Status.NOT_FOUND) : Response.ok(toJSON(attrs))).build();
+    }
+
+    private Response errResponse(String errorMessage, Response.Status status) {
+        return Response.status(status)
+                .type(MediaType.APPLICATION_JSON_TYPE)
+                .entity("{\"errorMessage\":\"" + errorMessage + "\"}")
+                .build();
+    }
+
+    private StreamingOutput toJSON(Attributes attrs) {
+        return out -> {
+            try (JsonGenerator gen = Json.createGenerator(out)) {
+                (new JSONWriter(gen)).write(attrs);
+            }
+        };
+    }
+}
---
+++ dcm4chee-arc-xroad/src/main/java/org/dcm4chee/arc/xroad/XRoadServiceProvider.java
@@ -0,0 +1,199 @@
+/*
+ * **** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is part of dcm4che, an implementation of DICOM(TM) in
+ * Java(TM), hosted at https://github.com/dcm4che.
+ *
+ * The Initial Developer of the Original Code is
+ * J4Care.
+ * Portions created by the Initial Developer are Copyright (C) 2015-2018
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ * See @authors listed below
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * **** END LICENSE BLOCK *****
+ *
+ */
+
+package org.dcm4chee.arc.xroad;
+
+import org.apache.cxf.configuration.jsse.TLSClientParameters;
+import org.apache.cxf.endpoint.Client;
+import org.apache.cxf.frontend.ClientProxy;
+import org.apache.cxf.transport.http.HTTPConduit;
+import org.dcm4che3.data.Attributes;
+import org.dcm4che3.data.Tag;
+import org.dcm4che3.data.VR;
+import org.dcm4che3.net.Device;
+import org.dcm4che3.util.StringUtils;
+import org.dcm4chee.arc.conf.ArchiveDeviceExtension;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.xml.ws.BindingProvider;
+import javax.xml.ws.Holder;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Gunter Zeilinger <gunterze@gmail.com>
+ * @since Feb 2018
+ */
+@ApplicationScoped
+public class XRoadServiceProvider {
+
+    @Inject
+    private Device device;
+
+    private XRoadService service = new XRoadService();
+
+    public Attributes rr441(String patientID) throws GeneralSecurityException, IOException, XRoadException {
+        Headers h = new Headers(arcdev(), "RR441");
+        Holder<RR441RequestType> request = new Holder<>(toRR441(patientID));
+        Holder<RR441ResponseType> response = new Holder<>();
+        port().rr441(request, h.client, h.service, h.userId, h.id, h.protocolVersion, response,  h.requestHash);
+        return toAttributes(XRoadException.validate(response.value));
+    }
+
+    private RR441RequestType toRR441(String patientID) {
+        RR441RequestType rq = new RR441RequestType();
+        rq.setCValjad(arcdev().getXRoadProperty("rr441.cValjad", "1,2,6,7,9,10"));
+        rq.setCIsikukoodid(patientID);
+        return rq;
+    }
+
+    private Attributes toAttributes(RR441ResponseType rsp) {
+        List<RR441ResponseType.TtIsikuid.TtIsikud> ttIsikudList = rsp.getTtIsikuid().getTtIsikud();
+        if (ttIsikudList.isEmpty())
+            return null;
+
+        RR441ResponseType.TtIsikuid.TtIsikud ttIsikud = ttIsikudList.get(0);
+        Attributes attrs = new Attributes();
+        attrs.setString(Tag.SpecificCharacterSet, VR.CS, arcdev().getDefaultCharacterSet());
+        attrs.setString(Tag.PatientName, VR.PN, ttIsikud.getTtIsikudCPerenimi() + '^' + ttIsikud.getTtIsikudCEesnimi());
+        attrs.setString(Tag.PatientID, VR.LO, ttIsikud.getTtIsikudCIsikukood());
+        attrs.setString(Tag.PatientSex, VR.CS, patientSex(ttIsikud.getTtIsikudCSugu()));
+        attrs.setString(Tag.PatientBirthDate, VR.DA, patientBirthDate(ttIsikud.getTtIsikudCSynniaeg()));
+        return attrs;
+    }
+
+    private String patientBirthDate(String synniaeg) {
+        if (synniaeg == null || synniaeg.length() != 10)
+            return null;
+
+        char[] data = new char[8];
+        synniaeg.getChars(6, 10, data, 0);
+        synniaeg.getChars(3, 5, data, 4);
+        synniaeg.getChars(0, 2, data, 6);
+        return new String(data);
+    }
+
+    private String patientSex(String sugu) {
+        if ("M".equals(sugu))
+            return "M";
+
+        if ("N".equals(sugu))
+            return "F";
+
+        return null;
+    }
+
+    private ArchiveDeviceExtension arcdev() {
+        return device.getDeviceExtension(ArchiveDeviceExtension.class);
+    }
+
+    private XRoadAdapterPortType port() throws GeneralSecurityException, IOException {
+        XRoadAdapterPortType port = service.getXRoadServicePort();
+        BindingProvider bindingProvider = (BindingProvider) port;
+        Map<String, Object> reqCtx = bindingProvider.getRequestContext();
+        String endpoint = arcdev().getXRoadProperty("endpoint", null);
+        reqCtx.put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, endpoint);
+        if (endpoint.startsWith("https")) {
+            Client client = ClientProxy.getClient(port);
+            HTTPConduit conduit = (HTTPConduit) client.getConduit();
+            conduit.setTlsClientParameters(tlsClientParams());
+        }
+        return port;
+    }
+
+    private TLSClientParameters tlsClientParams() throws GeneralSecurityException, IOException {
+        ArchiveDeviceExtension arcdev = arcdev();
+        TLSClientParameters params = new TLSClientParameters();
+        params.setKeyManagers(device.keyManagers());
+        params.setTrustManagers(device.trustManagers());
+        params.setSecureSocketProtocol(arcdev.getXRoadProperty("TLS.protocol", null));
+        for (String cipherSuite : StringUtils.split(
+                arcdev.getXRoadProperty("TLS.cipherSuites", null), ','))
+            params.getCipherSuites().add(cipherSuite.trim());
+        params.setDisableCNCheck(
+                Boolean.parseBoolean(arcdev.getXRoadProperty("TLS.disableCNCheck", null)));
+        return params;
+    }
+
+    private static class Headers {
+        final Holder<XRoadClientIdentifierType> client = new Holder<>();
+        final Holder<XRoadServiceIdentifierType> service = new Holder<>();
+        final Holder<String> userId = new Holder<>();
+        final Holder<String> id = new Holder<>();
+        final Holder<String> protocolVersion = new Holder<>();
+        final Holder<RequestHash> requestHash = new Holder<>();
+
+        private Headers(ArchiveDeviceExtension arcdev, String serviceCode) {
+            client.value = client(arcdev);
+            service.value = service(arcdev, serviceCode);
+            userId.value = arcdev.getXRoadProperty("userId", "EE11111111111");
+            id.value = arcdev.getXRoadProperty("id", null);
+            protocolVersion.value = arcdev.getXRoadProperty("protocolVersion", "4.0");
+        }
+
+        private static XRoadClientIdentifierType client(ArchiveDeviceExtension arcdev) {
+            XRoadClientIdentifierType type = new XRoadClientIdentifierType();
+            type.setObjectType(XRoadObjectType.valueOf(
+                    arcdev.getXRoadProperty("client.objectType", XRoadObjectType.SUBSYSTEM.name())));
+            type.setXRoadInstance(arcdev.getXRoadProperty("client.xRoadInstance", "ee-test"));
+            type.setMemberClass(arcdev.getXRoadProperty("client.memberClass", "NGO"));
+            type.setMemberCode(arcdev.getXRoadProperty("client.memberCode", "90007945"));
+            type.setSubsystemCode(arcdev.getXRoadProperty("client.subsystemCode", "mia"));
+            return type;
+        }
+
+        private static XRoadServiceIdentifierType service(ArchiveDeviceExtension arcdev, String serviceCode) {
+            XRoadServiceIdentifierType type = new XRoadServiceIdentifierType();
+            type.setObjectType(XRoadObjectType.valueOf(
+                    arcdev.getXRoadProperty("service.objectType", XRoadObjectType.SERVICE.name())));
+            type.setXRoadInstance(arcdev.getXRoadProperty("service.xRoadInstance", "ee-test"));
+            type.setMemberClass(arcdev.getXRoadProperty("service.memberClass", "GOV"));
+            type.setMemberCode(arcdev.getXRoadProperty("service.memberCode", "70008440"));
+            type.setSubsystemCode(arcdev.getXRoadProperty("service.subsystemCode", "rr"));
+            type.setServiceCode(serviceCode);
+            type.setServiceVersion(arcdev.getXRoadProperty("serviceVersion", "v1"));
+            return type;
+        }
+    }
+}
