c4f9ad270b5e3217e193e1741a139ff9480d8803
shefki <shralsheki@gmail.com>
2018-06-05 17:53:31 +0200

    UI Monitoring : Enable to show list of Diff Tasks #1392

--- dcm4chee-arc-ui2/src/app/monitoring/diff-monitor/diff-monitor.component.ts
+++ dcm4chee-arc-ui2/src/app/monitoring/diff-monitor/diff-monitor.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import {DiffMonitorService} from "./diff-monitor.service";
+import {AppService} from "../../app.service";
+import {ActivatedRoute} from "@angular/router";
+import * as _ from 'lodash';
+import {LoadingBarService} from "@ngx-loading-bar/core";
+import {AeListService} from "../../ae-list/ae-list.service";
+import {Observable} from "rxjs/Observable";
+import {j4care} from "../../helpers/j4care.service";
 
 @Component({
   selector: 'diff-monitor',
@@ -7,9 +15,79 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiffMonitorComponent implements OnInit {
 
-  constructor() { }
+    filterObject = {};
+    filterSchema;
+    urlParam;
+    aes;
+    aets;
+    devices;
+    constructor(
+    private service:DiffMonitorService,
+    private mainservice:AppService,
+    private route: ActivatedRoute,
+    public cfpLoadingBar: LoadingBarService,
+    public aeListService:AeListService,
+    ) { }
 
-  ngOnInit() {
-  }
+    ngOnInit(){
+      this.initCheck(10);
+    }
+    initCheck(retries){
+      let $this = this;
+      if(_.hasIn(this.mainservice,"global.authentication") || (_.hasIn(this.mainservice,"global.notSecure") && this.mainservice.global.notSecure)){
 
+          this.route.queryParams.subscribe(params => {
+              console.log("params",params);
+              this.urlParam = Object.assign({},params);
+              this.init();
+          });
+      }else{
+          if (retries){
+              setTimeout(()=>{
+                  $this.initCheck(retries-1);
+              },20);
+          }else{
+              this.init();
+          }
+      }
+    }
+
+    init(){
+      this.filterObject = {
+          limit:20
+      };
+      Observable.forkJoin(
+          this.aeListService.getAes(),
+          this.aeListService.getAets(),
+          this.service.getDevices()
+      ).subscribe((response)=>{
+          this.aes = (<any[]>j4care.extendAetObjectWithAlias(response[0])).map(ae => {
+              return {
+                  value:ae.dicomAETitle,
+                  text:ae.dicomAETitle
+              }
+          });
+          this.aets = (<any[]>j4care.extendAetObjectWithAlias(response[1])).map(ae => {
+              return {
+                  value:ae.dicomAETitle,
+                  text:ae.dicomAETitle
+              }
+          });
+          this.devices = (<any[]>response[2])
+              .filter(dev => dev.hasArcDevExt)
+              .map(device => {
+                  return {
+                      value:device.dicomDeviceName,
+                      text:device.dicomDeviceName
+                  }
+              });
+          this.initSchema();
+      });
+    }
+
+    initSchema(){
+      this.filterSchema = j4care.prepareFlatFilterObject(this.service.getFormSchema(this.aes, this.aets,"Size",this.devices),3);
+    }
+    ngOnDestroy(){
+    }
 }
--- dcm4chee-arc-ui2/src/app/monitoring/diff-monitor/diff-monitor.service.ts
+++ dcm4chee-arc-ui2/src/app/monitoring/diff-monitor/diff-monitor.service.ts
@@ -1,8 +1,163 @@
 import { Injectable } from '@angular/core';
+import {DevicesService} from "../../devices/devices.service";
 
 @Injectable()
 export class DiffMonitorService {
 
-  constructor() { }
+    constructor(
+        private deviceService: DevicesService
+    ) { }
 
+    statusValues(){
+        return [
+            {
+                value:"SCHEDULED",
+                text:"SCHEDULED"
+            },
+            {
+                value:"IN PROCESS",
+                text:"IN PROCESS"
+            },
+            {
+                value:"COMPLETED",
+                text:"COMPLETED"
+            },
+            {
+                value:"WARNING",
+                text:"WARNING"
+            },
+            {
+                value:"FAILED",
+                text:"FAILED"
+            },
+            {
+                value:"CANCELED",
+                text:"CANCELED"
+            }
+        ];
+    }
+
+  getFormSchema(aes, aets, countText, devices){
+      return [
+          {
+              tag:"select",
+              options:devices,
+              showStar:true,
+              filterKey:"dicomDeviceName",
+              description:"Device Name",
+              placeholder:"Device Name"
+          },{
+              tag:"select",
+              options:aes,
+              showStar:true,
+              filterKey:"LocalAET",
+              description:"Local AET",
+              placeholder:"Local AET"
+          },{
+              tag:"select",
+              options:aets,
+              showStar:true,
+              filterKey:"PrimaryAET",
+              description:"Primary AET",
+              placeholder:"Primary AET"
+          },
+          {
+              tag:"select",
+              options:aets,
+              showStar:true,
+              filterKey:"SecondaryAET",
+              description:"Secondary AET",
+              placeholder:"Secondary AET"
+          },
+          {
+              tag:"checkbox",
+              filterKey:"checkMissing",
+              description:"Check Missing",
+              text:"Check Missing"
+          },{
+              tag:"checkbox",
+              filterKey:"checkDifferent",
+              description:"Check Different",
+              text:"Check Different"
+          },
+          {
+              tag:"input",
+              type:"text",
+              filterKey:"comparefield",
+              description:"Compare field",
+              placeholder:"Compare field"
+          },{
+              tag:"select",
+              options:this.statusValues(),
+              filterKey:"status",
+              showStar:true,
+              description:"Status of tasks to filter by",
+              placeholder:"Status"
+          },
+          {
+              tag:"range-picker",
+              filterKey:"createdTime",
+              description:"Created Date"
+          },{
+              tag:"range-picker",
+              filterKey:"updatedTime",
+              description:"Created Date"
+          },
+          {
+              tag:"input",
+              type:"text",
+              filterKey:"batchID",
+              description:"Batch ID",
+              placeholder:"Batch ID"
+          },{
+              tag:"select",
+              options:[{
+                  value:'createdTime',
+                  text:'Sort by creation time (ASC)'
+              },
+                  {
+                      value:'-createdTime',
+                      text:'Sort by creation time (DESC)'
+                  },
+                  {
+                      value:'updatedTime',
+                      text:'Sort by updated time (ASC)'
+                  },
+                  {
+                      value:'-updatedTime',
+                      text:'Sort by updated time (DESC)'
+                  }
+              ],
+              showStar:true,
+              filterKey:"orderby",
+              description:"Sort",
+              placeholder:"Sort"
+          },
+          {
+              tag:"label",
+              text:"Page Size"
+          },{
+              tag:"input",
+              type:"number",
+              filterKey:"limit",
+              description:"Page Size",
+              placeholder:"Page Size"
+          },
+          {
+              tag:"button",
+              text:countText,
+              id:"count",
+              description:"Get Count"
+          },
+          {
+              tag:"button",
+              text:"Search",
+              id:"search",
+              description:"Search Patients"
+          }
+      ];
+  }
+    getDevices(){
+        return this.deviceService.getDevices()
+    }
 }
