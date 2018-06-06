b34699765d2cce25ec45475dc0ee179000f001f7
Gunter Zeilinger <gunterze@gmail.com>
2018-06-06 15:56:23 +0200

    Retrieve Cache for Nearline Storage #1402

--- dcm4chee-arc-store-scu/src/main/java/org/dcm4chee/arc/store/scu/impl/RetrieveTaskImpl.java
+++ dcm4chee-arc-store-scu/src/main/java/org/dcm4chee/arc/store/scu/impl/RetrieveTaskImpl.java
@@ -256,13 +256,13 @@ final class RetrieveTaskImpl implements RetrieveTask {
 
     private void waitForOutstandingCStoreRSP() {
         try {
-            LOG.info("{}: wait for outstanding C-STORE RSP(s) on association to {}",
+            LOG.debug("{}: wait for outstanding C-STORE RSP(s) on association to {}",
                     rqas, storeas.getRemoteAET());
             synchronized (outstandingRSP) {
                 while (!outstandingRSP.isEmpty())
                     outstandingRSP.wait();
             }
-            LOG.info("{}: received outstanding C-STORE RSP(s) on association to {}",
+            LOG.debug("{}: received outstanding C-STORE RSP(s) on association to {}",
                     rqas, storeas.getRemoteAET());
         } catch (InterruptedException e) {
             LOG.warn("{}: failed to wait for outstanding C-STORE RSP(s) on association to {}",
