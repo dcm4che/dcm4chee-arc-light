019178959874475d7d746d8a8c3df447f52d0f21
Gunter Zeilinger <gunterze@gmail.com>
2018-07-26 10:16:34 +0200

    Large number of Audit Spool files may cause OutOfMemoryError fix #1499

--- dcm4chee-arc-audit/src/main/java/org/dcm4chee/arc/audit/AuditScheduler.java
+++ dcm4chee-arc-audit/src/main/java/org/dcm4chee/arc/audit/AuditScheduler.java
@@ -57,7 +57,6 @@ import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 
 /**
  * @author Gunter Zeilinger <gunterze@gmail.com>
