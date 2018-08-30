a9cc07d1564d975fc76d1a6064d5fcde8d37dd00
Gunter Zeilinger <gunterze@gmail.com>
2018-08-30 14:55:34 +0200

    increment version to 5.14.1

--- dcm4chee-arc-conf-test/src/test/java/org/dcm4chee/arc/conf/ArchiveDeviceFactory.java
+++ dcm4chee-arc-conf-test/src/test/java/org/dcm4chee/arc/conf/ArchiveDeviceFactory.java
@@ -981,7 +981,7 @@ class ArchiveDeviceFactory {
     };
 
     static final String AE_TITLE = "DCM4CHEE";
-    static final String DCM4CHEE_ARC_VERSION = "5.14.0";
+    static final String DCM4CHEE_ARC_VERSION = "5.14.1";
     static final String DCM4CHEE_ARC_KEY_JKS =  "${jboss.server.config.url}/dcm4chee-arc/key.jks";
     static final String HL7_ADT2DCM_XSL = "${jboss.server.temp.url}/dcm4chee-arc/hl7-adt2dcm.xsl";
     static final String HL7_DCM2ADT_XSL = "${jboss.server.temp.url}/dcm4chee-arc/hl7-dcm2adt.xsl";
