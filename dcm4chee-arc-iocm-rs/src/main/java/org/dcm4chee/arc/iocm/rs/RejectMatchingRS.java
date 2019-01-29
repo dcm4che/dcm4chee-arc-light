2cf4b425a333f11126d6152365500be7bd6f5964
vrinda <vrinda.nayak@j4care.com>
2019-01-29 12:55:11 +0100

    IOCM-RS: reject instances of matching entities #1808

---
+++ dcm4chee-arc-iocm-rs/src/main/java/org/dcm4chee/arc/iocm/rs/RejectMatchingRS.java
@@ -0,0 +1,395 @@
+/*
+ * *** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is part of dcm4che, an implementation of DICOM(TM) in
+ * Java(TM), hosted at https://github.com/dcm4che.
+ *
+ * The Initial Developer of the Original Code is
+ * J4Care.
+ * Portions created by the Initial Developer are Copyright (C) 2017-2019
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ * See @authors listed below
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * *** END LICENSE BLOCK *****
+ */
+
+package org.dcm4chee.arc.iocm.rs;
+
+import org.dcm4che3.data.*;
+import org.dcm4che3.net.ApplicationEntity;
+import org.dcm4che3.net.Device;
+import org.dcm4che3.net.service.QueryRetrieveLevel2;
+import org.dcm4chee.arc.conf.ArchiveDeviceExtension;
+import org.dcm4chee.arc.conf.RejectionNote;
+import org.dcm4chee.arc.delete.RejectionService;
+import org.dcm4chee.arc.entity.ExpirationState;
+import org.dcm4chee.arc.entity.Patient;
+import org.dcm4chee.arc.qmgt.HttpServletRequestInfo;
+import org.dcm4chee.arc.qmgt.QueueSizeLimitExceededException;
+import org.dcm4chee.arc.query.Query;
+import org.dcm4chee.arc.query.QueryContext;
+import org.dcm4chee.arc.query.QueryService;
+import org.dcm4chee.arc.query.util.QueryAttributes;
+import org.hibernate.Transaction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.constraints.Pattern;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+/**
+ * @author Vrinda Nayak <vrinda.nayak@j4care.com>
+ * @since Jan 2019
+ */
+@RequestScoped
+@Path("aets/{AETitle}/reject/{codeValue}^{codingSchemeDesignator}")
+public class RejectMatchingRS {
+
+    private static final Logger LOG = LoggerFactory.getLogger(RejectMatchingRS.class);
+
+    @Context
+    private HttpServletRequest request;
+
+    @Context
+    private UriInfo uriInfo;
+
+    @Inject
+    private Device device;
+
+    @Inject
+    private RejectionService rejectionService;
+
+    @Inject
+    private QueryService queryService;
+
+    @PathParam("AETitle")
+    private String aet;
+
+    @PathParam("codeValue")
+    String codeValue;
+
+    @PathParam("codingSchemeDesignator")
+    String designator;
+
+    @QueryParam("batchID")
+    private String batchID;
+
+    @QueryParam("fuzzymatching")
+    @Pattern(regexp = "true|false")
+    private String fuzzymatching;
+
+    @QueryParam("incomplete")
+    @Pattern(regexp = "true|false")
+    private String incomplete;
+
+    @QueryParam("retrievefailed")
+    @Pattern(regexp = "true|false")
+    private String retrievefailed;
+
+    @QueryParam("ExpirationDate")
+    private String expirationDate;
+
+    @QueryParam("storageVerificationFailed")
+    @Pattern(regexp = "true|false")
+    private String storageVerificationFailed;
+
+    @QueryParam("compressionfailed")
+    @Pattern(regexp = "true|false")
+    private String compressionfailed;
+
+    @QueryParam("ExternalRetrieveAET")
+    private String externalRetrieveAET;
+
+    @QueryParam("ExternalRetrieveAET!")
+    private String externalRetrieveAETNot;
+
+    @QueryParam("patientVerificationStatus")
+    @Pattern(regexp = "UNVERIFIED|VERIFIED|NOT_FOUND|VERIFICATION_FAILED")
+    private String patientVerificationStatus;
+
+    @QueryParam("storageID")
+    private String storageID;
+
+    @QueryParam("storageClustered")
+    @Pattern(regexp = "true|false")
+    private String storageClustered;
+
+    @QueryParam("storageExported")
+    @Pattern(regexp = "true|false")
+    private String storageExported;
+
+    @QueryParam("allOfModalitiesInStudy")
+    @Pattern(regexp = "true|false")
+    private String allOfModalitiesInStudy;
+
+    @QueryParam("StudySizeInKB")
+    @Pattern(regexp = "\\d{1,6}(-\\d{0,6})?|-\\d{1,6}")
+    private String studySizeInKB;
+
+    @QueryParam("ExpirationState")
+    @Pattern(regexp = "UPDATEABLE|FROZEN|REJECTED|EXPORT_SCHEDULED|FAILED_TO_EXPORT|FAILED_TO_REJECT")
+    private String expirationState;
+
+    @POST
+    @Path("/studies")
+    @Produces("application/json")
+    public Response rejectMatchingStudies() {
+        return rejectMatching(
+                "rejectMatchingStudies",
+                QueryRetrieveLevel2.STUDY,
+                null,
+                null);
+    }
+
+    @POST
+    @Path("/series")
+    @Produces("application/json")
+    public Response rejectMatchingSeries() {
+        return rejectMatching(
+                "rejectMatchingSeries",
+                QueryRetrieveLevel2.SERIES,
+                null,
+                null);
+    }
+
+    @POST
+    @Path("/studies/{StudyInstanceUID}/series")
+    @Produces("application/json")
+    public Response rejectMatchingSeriesOfStudy(
+            @PathParam("StudyInstanceUID") String studyInstanceUID){
+        return rejectMatching(
+                "rejectMatchingSeriesOfStudy",
+                QueryRetrieveLevel2.SERIES,
+                studyInstanceUID,
+                null);
+    }
+
+    @POST
+    @Path("/instances")
+    @Produces("application/json")
+    public Response rejectMatchingInstances() {
+        return rejectMatching(
+                "rejectMatchingInstances",
+                QueryRetrieveLevel2.IMAGE,
+                null,
+                null);
+    }
+
+    @POST
+    @Path("/studies/{StudyInstanceUID}/instances")
+    @Produces("application/json")
+    public Response rejectMatchingInstancesOfStudy(
+            @PathParam("StudyInstanceUID") String studyInstanceUID) {
+        return rejectMatching(
+                "rejectMatchingInstancesOfStudy",
+                QueryRetrieveLevel2.IMAGE, studyInstanceUID,
+                null);
+    }
+
+    @Path("/studies/{StudyInstanceUID}/series/{SeriesInstanceUID}/instances")
+    @Produces("application/json")
+    public Response rejectMatchingInstancesOfSeries(
+            @PathParam("StudyInstanceUID") String studyInstanceUID,
+            @PathParam("SeriesInstanceUID") String seriesInstanceUID) {
+        return rejectMatching(
+                "rejectMatchingInstancesOfSeries",
+                QueryRetrieveLevel2.IMAGE,
+                studyInstanceUID,
+                seriesInstanceUID);
+    }
+
+    private Response rejectMatching(
+            String method, QueryRetrieveLevel2 qrlevel, String studyInstanceUID, String seriesInstanceUID) {
+        LOG.info("Process POST {}?{} from {}@{}",
+                request.getRequestURI(),
+                request.getQueryString(),
+                request.getRemoteUser(),
+                request.getRemoteHost());
+        ApplicationEntity ae = device.getApplicationEntity(aet, true);
+        if (ae == null || !ae.isInstalled())
+            return errResponse("No such Application Entity: " + aet, Response.Status.NOT_FOUND);
+
+        Code rjNoteCode = toRejectionNote(codeValue, designator).getRejectionNoteCode();
+        try {
+            QueryContext ctx = queryContext(method, qrlevel, studyInstanceUID, seriesInstanceUID, ae);
+            String warning = null;
+            int count = 0;
+            Response.Status status = Response.Status.ACCEPTED;
+            HttpServletRequestInfo httpRequestInfo = HttpServletRequestInfo.valueOf(request);
+            try (Query query = queryService.createQuery(ctx)) {
+                query.initQuery();
+                Transaction transaction = query.beginTransaction();
+                try {
+                    query.setFetchSize(arcDev().getQueryFetchSize());
+                    query.executeQuery();
+                    while (query.hasMoreMatches()) {
+                        Attributes match = query.nextMatch();
+                        rejectMatching(rjNoteCode, match, qrlevel, httpRequestInfo);
+                        count++;
+                    }
+                } catch (QueueSizeLimitExceededException e) {
+                    status = Response.Status.SERVICE_UNAVAILABLE;
+                    warning = e.getMessage();
+                } catch (Exception e) {
+                    warning = e.getMessage();
+                    status = Response.Status.INTERNAL_SERVER_ERROR;
+                } finally {
+                    try {
+                        transaction.commit();
+                    } catch (Exception e) {
+                        LOG.warn("Failed to commit transaction:\n{}", e);
+                    }
+                }
+            }
+            Response.ResponseBuilder builder = Response.status(status);
+            if (warning != null)
+                builder.header("Warning", warning);
+            return builder.entity("{\"count\":" + count + '}').build();
+        } catch (Exception e) {
+            return errResponseAsTextPlain(e);
+        }
+    }
+
+    private void rejectMatching(Code rjNoteCode, Attributes match, QueryRetrieveLevel2 qrlevel,
+                                HttpServletRequestInfo httpRequestInfo)
+            throws QueueSizeLimitExceededException {
+        rejectionService.scheduleReject(aet,
+                match.getString(Tag.StudyInstanceUID),
+                qrlevel != QueryRetrieveLevel2.STUDY ? match.getString(Tag.SeriesInstanceUID) : null,
+                qrlevel == QueryRetrieveLevel2.IMAGE ? match.getString(Tag.SOPInstanceUID) : null,
+                rjNoteCode,
+                httpRequestInfo,
+                batchID);
+    }
+
+    private RejectionNote toRejectionNote(String codeValue, String designator) {
+        if (codeValue == null)
+            return null;
+
+        RejectionNote rjNote = arcDev().getRejectionNote(
+                new Code(codeValue, designator, null, ""));
+
+        if (rjNote == null)
+            throw new WebApplicationException(errResponse("Unknown Rejection Note Code: ("
+                    + codeValue + ", " + designator + ')', Response.Status.NOT_FOUND));
+
+        return rjNote;
+    }
+
+    private QueryContext queryContext(
+            String method, QueryRetrieveLevel2 qrlevel, String studyInstanceUID, String seriesInstanceUID,
+            ApplicationEntity ae) {
+        QueryContext ctx = queryService.newQueryContextQIDO(request, method, ae, queryParam(ae));
+        ctx.setQueryRetrieveLevel(qrlevel);
+        QueryAttributes queryAttrs = new QueryAttributes(uriInfo, null);
+        Attributes keys = queryAttrs.getQueryKeys();
+        IDWithIssuer idWithIssuer = IDWithIssuer.pidOf(keys);
+        if (idWithIssuer != null)
+            ctx.setPatientIDs(idWithIssuer);
+        if (studyInstanceUID != null)
+            keys.setString(Tag.StudyInstanceUID, VR.UI, studyInstanceUID);
+        if (seriesInstanceUID != null)
+            keys.setString(Tag.SeriesInstanceUID, VR.UI, seriesInstanceUID);
+        ctx.setQueryKeys(keys);
+        Attributes returnKeys = new Attributes(3);
+        returnKeys.setNull(Tag.StudyInstanceUID, VR.UI);
+        switch (qrlevel) {
+            case IMAGE:
+                returnKeys.setNull(Tag.SOPInstanceUID, VR.UI);
+            case SERIES:
+                returnKeys.setNull(Tag.SeriesInstanceUID, VR.UI);
+        }
+        ctx.setReturnKeys(returnKeys);
+        return ctx;
+    }
+
+    private org.dcm4chee.arc.query.util.QueryParam queryParam(ApplicationEntity ae) {
+        org.dcm4chee.arc.query.util.QueryParam queryParam = new org.dcm4chee.arc.query.util.QueryParam(ae);
+        queryParam.setCombinedDatetimeMatching(true);
+        queryParam.setFuzzySemanticMatching(Boolean.parseBoolean(fuzzymatching));
+        queryParam.setAllOfModalitiesInStudy(Boolean.parseBoolean(allOfModalitiesInStudy));
+        queryParam.setIncomplete(Boolean.parseBoolean(incomplete));
+        queryParam.setRetrieveFailed(Boolean.parseBoolean(retrievefailed));
+        queryParam.setStorageVerificationFailed(Boolean.parseBoolean(storageVerificationFailed));
+        queryParam.setCompressionFailed(Boolean.parseBoolean(compressionfailed));
+        queryParam.setExternalRetrieveAET(externalRetrieveAET);
+        queryParam.setExternalRetrieveAETNot(externalRetrieveAETNot);
+        queryParam.setExpirationDate(expirationDate);
+        if (patientVerificationStatus != null)
+            queryParam.setPatientVerificationStatus(Patient.VerificationStatus.valueOf(patientVerificationStatus));
+        if (storageID != null)
+            queryParam.setStudyStorageIDs(device.getDeviceExtensionNotNull(ArchiveDeviceExtension.class)
+                    .getStudyStorageIDs(storageID, parseBoolean(storageClustered), parseBoolean(storageExported)));
+        queryParam.setStudySizeRange(studySizeInKB);
+        if (expirationState != null)
+            queryParam.setExpirationState(ExpirationState.valueOf(expirationState));
+        return queryParam;
+    }
+
+    private static Boolean parseBoolean(String s) {
+        return s != null ? Boolean.valueOf(s) : null;
+    }
+
+    private ArchiveDeviceExtension arcDev() {
+        try {
+            return device.getDeviceExtensionNotNull(ArchiveDeviceExtension.class);
+        } catch (IllegalStateException e) {
+            throw new WebApplicationException(
+                    errResponse("Archive Device Extension not configured for device: "
+                                    + device.getDeviceName(),
+                            Response.Status.NOT_FOUND));
+        }
+    }
+
+    private static Response errResponse(String message, Response.Status status) {
+        return Response.status(status)
+                .entity("{\"errorMessage\":\"" + message + "\"}")
+                .build();
+    }
+
+    private Response errResponseAsTextPlain(Exception e) {
+        return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                .entity(exceptionAsString(e))
+                .type("text/plain")
+                .build();
+    }
+
+    private String exceptionAsString(Exception e) {
+        StringWriter sw = new StringWriter();
+        e.printStackTrace(new PrintWriter(sw));
+        return sw.toString();
+    }
+}
