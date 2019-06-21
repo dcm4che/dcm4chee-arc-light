d53a7c16ce6273ba31319f90ee6965fef11037c1
Gunter Zeilinger <gunterze@gmail.com>
2019-06-21 11:34:42 +0200

    Expose Keycloak adapter configuration for UI application by restful service fix #2042

---
+++ dcm4chee-arc-conf-keycloak/src/main/java/org/dcm4chee/arc/conf/keycloak/KeycloakJSON.java
@@ -0,0 +1,74 @@
+/*
+ * **** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is part of dcm4che, an implementation of DICOM(TM) in
+ * Java(TM), hosted at https://github.com/dcm4che.
+ *
+ * The Initial Developer of the Original Code is
+ * J4Care.
+ * Portions created by the Initial Developer are Copyright (C) 2015-2019
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ * See @authors listed below
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * **** END LICENSE BLOCK *****
+ *
+ */
+
+package org.dcm4chee.arc.conf.keycloak;
+
+import org.jboss.resteasy.annotations.cache.NoCache;
+
+import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+
+/**
+ * @author Gunter Zeilinger <gunterze@gmail.com>
+ * @since Jun 2019
+ */
+@RequestScoped
+@Path("keycloak.json")
+public class KeycloakJSON {
+    @GET
+    @NoCache
+    @Produces("application/json")
+    public String get() {
+        String authServerURL = System.getProperty("auth-server-url");
+        if (authServerURL == null) {
+            throw new WebApplicationException(Response.Status.NOT_FOUND);
+        }
+        return "{\"realm\":\"" + System.getProperty("realm-name", "dcm4che") +
+                "\",\"resource\":\"" + System.getProperty("ui-client-id","dcm4chee-arc-ui") +
+                "\",\"auth-server-url\":\"" + authServerURL +
+                "\",\"ssl-required\":\"" + System.getProperty("ssl-required","external") +
+                "\",\"public-client\":true,\"confidential-port\":0}";
+    }
+}
