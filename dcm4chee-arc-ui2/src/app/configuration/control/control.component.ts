aeca137192f02c1ffecde29b84524fd6b35cc59c
shefki <shralsheki@gmail.com>
2019-07-02 14:39:32 +0200

    UI / Studies: Accepted Roles in the AET missing [redmine] #11114

--- dcm4chee-arc-ui2/src/app/configuration/control/control.component.ts
+++ dcm4chee-arc-ui2/src/app/configuration/control/control.component.ts
@@ -27,7 +27,7 @@ export class ControlComponent implements OnInit{
     }
     initCheck(retries){
         let $this = this;
-        if(_.hasIn(this.appservices,"user.roles") || (_.hasIn(this.appservices,"global.notSecure") && this.appservices.global.notSecure)){
+        if(KeycloakService.keycloakAuth.authenticated || (_.hasIn(this.appservices,"global.notSecure") && this.appservices.global.notSecure)){
             this.init();
         }else{
             if (retries){
