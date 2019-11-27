1cd96ba742ad1900bee12499fd1b21439eb37ffa
Vrinda <vrinda.nayak@j4care.com>
2019-11-27 15:17:30 +0100

    SQL update scripts for h2, db2, mysql and sqlserver shall NOT create sequences #2278

--- dcm4chee-arc-entity/src/main/resources/sql/update-5.6-sqlserver.sql
+++ dcm4chee-arc-entity/src/main/resources/sql/update-5.6-sqlserver.sql
@@ -1,35 +1,33 @@
 alter table patient add num_studies int;
 update patient set num_studies = (
-  select count(*) from study where study.patient_fk=patient.pk and study.rejection_state in (0,1));
+    select count(*) from study where study.patient_fk=patient.pk and study.rejection_state in (0,1));
 alter table patient alter column num_studies int not null;
 create index UK_296rccryifu6d8byisl2f4dvq on patient (num_studies);
 
-alter table location add multi_ref int, add uidmap_fk bigint, add object_type int;
+alter table location add multi_ref int;
+alter table location add uidmap_fk bigint;
+alter table location add object_type int;
 update location set object_type = 0;
 alter table location alter column object_type int not null;
 alter table location alter column tsuid varchar(255) null;
 create table uidmap (pk bigint identity not null, uidmap image not null, primary key (pk));
 
---to be checked---
 alter table instance add inst_no_int int;
-update instance set inst_no_int = inst_no::int where inst_no != '*';
+update instance set inst_no_int = inst_no where inst_no != '*';
 update instance set inst_no_int = null where inst_no = '*';
-alter table instance drop inst_no;
-alter table instance rename column inst_no_int to inst_no;
+drop index UK_ouh6caecancvsa05lknojy30j on instance;
+alter table instance drop column inst_no;
+exec sp_rename 'instance.inst_no_int', inst_no, 'COLUMN';
+
 alter table series add series_no_int int;
-update series set series_no_int = series_no::int where series_no != '*';
+update series set series_no_int = series_no where series_no != '*';
 update series set series_no_int = null where series_no = '*';
-alter table series drop series_no;
-alter table series rename column series_no_int to series_no;
---to be checked---
+drop index UK_75oc6w5ootkuwyvmrhe3tbown on series;
+alter table series drop column series_no;
+exec sp_rename 'series.series_no_int', series_no, 'COLUMN';
 
 create index UK_i1lnahmehau3r3j9pdyxg3p3y on location (multi_ref);
 alter table location add constraint FK_bfk5vl6eoxaf0hhwiu3rbgmkn foreign key (uidmap_fk) references uidmap;
-
---to be checked--
-create index FK_bfk5vl6eoxaf0hhwiu3rbgmkn on location (uidmap_fk) ;
-create sequence uidmap_pk_seq;
---to be checked--
 
 create index UK_j6aadbh7u93bpmv18s1inrl1r on series (failed_retrieves);
 create index UK_9qvng5j8xnli8yif7p0rjngb2 on study (failed_retrieves);
