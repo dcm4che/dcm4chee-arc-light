e67a1bd9c767aa58d11c734e40008ee6c0625ec3
shefki <shralsheki@gmail.com>
2019-12-16 15:28:17 +0100

    UI: Rename Menu item Studies to Navigation #2308

--- dcm4chee-arc-ui2/src/app/study/study-tab.component.ts
+++ dcm4chee-arc-ui2/src/app/study/study-tab.component.ts
@@ -4,11 +4,11 @@ import { Component, OnInit } from '@angular/core';
     selector: 'study-tab',
     template:`
         <ul class="nav nav-tabs" role="tablist">
-            <li [permission]="{id:'tab-study-study',param:'visible'}" role="presentation" routerLinkActive="active"><a role="tab" routerLink="/study/study" routerLinkActive="active">Study</a></li>
-            <li [permission]="{id:'tab-study-patient',param:'visible'}" role="presentation" routerLinkActive="active"><a role="tab" routerLink="/study/patient" routerLinkActive="active">Patient</a></li>
-            <li [permission]="{id:'tab-study-mwl',param:'visible'}" role="presentation" routerLinkActive="active"><a role="tab" routerLink="/study/mwl" routerLinkActive="active">MWL</a></li>
-            <li [permission]="{id:'tab-study-uwl',param:'visible'}" role="presentation" routerLinkActive="active"><a role="tab" routerLink="/study/uwl" routerLinkActive="active">UWL</a></li>
-            <li [permission]="{id:'tab-study-diff',param:'visible'}" role="presentation" routerLinkActive="active"><a role="tab" routerLink="/study/diff" routerLinkActive="active">DIFF</a></li>
+            <li [permission]="{id:'tab-study-study',param:'visible'}" role="presentation" routerLinkActive="active"><a role="tab" routerLink="/study/study" routerLinkActive="active">Studies</a></li>
+            <li [permission]="{id:'tab-study-patient',param:'visible'}" role="presentation" routerLinkActive="active"><a role="tab" routerLink="/study/patient" routerLinkActive="active">Patients</a></li>
+            <li [permission]="{id:'tab-study-mwl',param:'visible'}" role="presentation" routerLinkActive="active"><a role="tab" routerLink="/study/mwl" routerLinkActive="active">MWLs</a></li>
+            <li [permission]="{id:'tab-study-uwl',param:'visible'}" role="presentation" routerLinkActive="active"><a role="tab" routerLink="/study/uwl" routerLinkActive="active">UWLs</a></li>
+            <li [permission]="{id:'tab-study-diff',param:'visible'}" role="presentation" routerLinkActive="active"><a role="tab" routerLink="/study/diff" routerLinkActive="active">DIFFs</a></li>
         </ul>
     `
 })
