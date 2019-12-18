daabd8c124ea919d279a925b8b4bb2bcd45a7984
Gunter Zeilinger <gunterze@protonmail.com>
2019-12-18 11:32:53 +0100

    Extend Dictionary for Private Creator: ELSCINT1 dcm4che/dcm4che#602

--- dcm4chee-arc-ui2/src/app/constants/elscint-dict-names.js
+++ dcm4chee-arc-ui2/src/app/constants/elscint-dict-names.js
@@ -1,5 +1,13 @@
 DCM4CHE.elementName.addDictionary({
 "privateCreator":"ELSCINT1",
+"00E1xx21":"DLP",
 "00E1xx24":"Presentation Horizontal Invert",
-"00E1xx25":"Presentation Vertical Invert"
+"00E1xx25":"Presentation Vertical Invert",
+"00E1xx26":"Phantom Type",
+"00E1xx50":"Acquisition Duration",
+"00F1xx01":"Acquisition Type",
+"00F1xx07":"Table Velocity",
+"00F1xx26":"Pitch",
+"00F1xx27":"Rotation Time",
+"6001xx90":"Overlay Label"
 });
