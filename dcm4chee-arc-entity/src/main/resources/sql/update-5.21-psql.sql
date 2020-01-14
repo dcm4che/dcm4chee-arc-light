9841f79158affd80d16eaf256d5674de82b3ade5
Gunter Zeilinger <gunterze@protonmail.com>
2020-01-14 10:54:39 +0100

    Make Scheduled Time of Export Tasks optional #2371

--- dcm4chee-arc-entity/src/main/resources/sql/update-5.21-psql.sql
+++ dcm4chee-arc-entity/src/main/resources/sql/update-5.21-psql.sql
@@ -2,8 +2,8 @@
 alter table retrieve_task
     add scheduled_time timestamp;
 
-alter  table export_task
-    alter scheduled_time set null;
+alter table export_task
+    alter scheduled_time drop not null;
 
 create index UK_rqp93vxrhyg09x3ck7vc1mawp on retrieve_task (scheduled_time);
 
