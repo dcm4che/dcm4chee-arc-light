92d319561f98c2848336a3cdfe60d6775e970cb2
Vrinda <vrinda.nayak@j4care.com>
2020-01-21 14:13:59 +0100

    Fix #2393 : Update DB scripts for 5.21.0 for databases other than postgres

---
+++ dcm4chee-arc-entity/src/main/resources/sql/update-5.21-oracle.sql
@@ -0,0 +1,14 @@
+-- part 1: can be applied on archive running archive 5.20
+alter table retrieve_task
+    add scheduled_time timestamp;
+
+alter table export_task modify scheduled_time null;
+
+create index UK_rqp93vxrhyg09x3ck7vc1mawp on retrieve_task (scheduled_time);
+
+-- part 2: shall be applied on stopped archive before starting 5.21
+update retrieve_task
+    set scheduled_time = (select queue_msg.scheduled_time from queue_msg
+    where queue_msg_fk = queue_msg.pk);
+
+-- part 3: can be applied on already running archive 5.21
