943347c049231dc13fd92687df4f64e25f46c7ea
shefki <shralsheki@gmail.com>
2020-02-20 12:31:07 +0100

    removing logs, UI: Adding Internationalization support (i18n) #2293

--- dcm4chee-arc-ui2/src/main.ts
+++ dcm4chee-arc-ui2/src/main.ts
@@ -8,13 +8,8 @@ import { AppModule } from './app/';
 import { loadTranslations } from '@angular/localize';
 import { getTranslations, ParsedTranslationBundle } from '@locl/core';
 
-console.log("language set",)
 
-// Get data
 let languageCode = localStorage.getItem('language_code');
-
-console.log("languageCode from localstorage",languageCode);
-console.log("environment.production",environment.production);
 
 if (environment.production) {
   enableProdMode();
@@ -23,11 +18,8 @@ if (environment.production) {
 if(languageCode && languageCode != "en"){
     getTranslations(`./assets/locale/${languageCode}.json`).then(
         (data: ParsedTranslationBundle) => {
-            console.warn(data);
             loadTranslations(data.translations as any);
-            console.log("after loadTranslation");
             import('./app/app.module').then(module => {
-                console.log("in then",module);
                 platformBrowserDynamic()
                     .bootstrapModule(module.AppModule)
                     .catch(err => console.error(err));
