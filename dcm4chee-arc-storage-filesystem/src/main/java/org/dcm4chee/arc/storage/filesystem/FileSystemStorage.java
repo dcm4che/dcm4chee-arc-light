3e9d1fa426f308b42b1fc4a43528c1ae617d10eb
Gunter Zeilinger <gunterze@protonmail.com>
2020-04-23 11:50:24 +0200

    FileSystemStorage: optionally use alternative Files.createDirectories(Path,..) method

--- dcm4chee-arc-storage-filesystem/src/main/java/org/dcm4chee/arc/storage/filesystem/FileSystemStorage.java
+++ dcm4chee-arc-storage-filesystem/src/main/java/org/dcm4chee/arc/storage/filesystem/FileSystemStorage.java
@@ -54,6 +54,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URI;
 import java.nio.file.*;
+import java.nio.file.attribute.FileAttribute;
 import java.util.concurrent.ThreadLocalRandom;
 
 /**
@@ -68,6 +69,12 @@ public class FileSystemStorage extends AbstractStorage {
     private final AttributesFormat pathFormat;
     private final Path checkMountFilePath;
     private final OpenOption[] openOptions;
+    private final CreateDirectories createDirectories;
+
+    @FunctionalInterface
+    private interface CreateDirectories {
+        Path apply(Path dir, FileAttribute<?>... attrs) throws IOException;
+    }
 
     public FileSystemStorage(StorageDescriptor descriptor, MetricsService metricsService) {
         super(descriptor, metricsService);
@@ -79,6 +86,35 @@ public class FileSystemStorage extends AbstractStorage {
         openOptions = fileOpenOption != null
                 ? new OpenOption[]{ StandardOpenOption.CREATE_NEW, StandardOpenOption.valueOf(fileOpenOption) }
                 : new OpenOption[]{ StandardOpenOption.CREATE_NEW };
+        createDirectories = Boolean.parseBoolean(descriptor.getProperty("altCreateDirectories", null))
+            ? FileSystemStorage::altCreateDirectories
+            : Files::createDirectories;
+    }
+
+    private static Path altCreateDirectories(Path path, FileAttribute<?>... fileAttributes)
+            throws IOException {
+        try {
+            return Files.createDirectory(path, fileAttributes);
+        } catch (FileAlreadyExistsException e) {
+            return path;
+        } catch (NoSuchFileException e) {
+            createParentDirectories(path, fileAttributes);
+            return Files.createDirectory(path, fileAttributes);
+        }
+    }
+
+    private static void createParentDirectories(Path path, FileAttribute<?>... fileAttributes)
+            throws IOException {
+        Path parent = path.getParent();
+        if (parent == null)
+            throw new FileSystemException(path.toString(), null,
+                    "Unable to determine if root directory exists");
+        try {
+            Files.createDirectory(parent, fileAttributes);
+        } catch (NoSuchFileException e) {
+            createParentDirectories(parent, fileAttributes);
+            Files.createDirectory(parent, fileAttributes);
+        }
     }
 
     private URI ensureTrailingSlash(URI uri) {
@@ -110,7 +146,7 @@ public class FileSystemStorage extends AbstractStorage {
 
     private FileStore getFileStore() throws IOException {
         Path dir = Paths.get(rootURI);
-        Files.createDirectories(dir);
+        createDirectories.apply(dir);
         return Files.getFileStore(dir);
     }
 
@@ -123,7 +159,7 @@ public class FileSystemStorage extends AbstractStorage {
     protected OutputStream openOutputStreamA(WriteContext ctx) throws IOException {
         Path path = Paths.get(rootURI.resolve(pathFormat.format(ctx.getAttributes())));
         Path dir = path.getParent();
-        Files.createDirectories(dir);
+        createDirectories.apply(dir);
         OutputStream stream = null;
         while (stream == null)
             try {
