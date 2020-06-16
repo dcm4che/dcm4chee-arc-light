1c248d5bcd72c78058da3c8fd7aa655661dd27d5
Gunter Zeilinger <gunterze@protonmail.com>
2020-06-16 11:57:27 +0200

    dcm4che/dcm4che#743: dcm4che-net:WebApplication: Add ServiceClass XDS_RS for proprietary RESTful proxy for IHE XDS services

--- dcm4chee-arc-conf-rs/src/main/java/org/dcm4chee/arc/conf/rs/ConfigurationRS.java
+++ dcm4chee-arc-conf-rs/src/main/java/org/dcm4chee/arc/conf/rs/ConfigurationRS.java
@@ -115,7 +115,8 @@ public class ConfigurationRS {
             "MOVE|" +
             "MOVE_MATCHING|" +
             "REJECT|" +
-            "ELASTICSEARCH")
+            "ELASTICSEARCH|" +
+            "XDS_RS")
     private String dcmWebServiceClass;
 
     private ConfigurationDelegate configDelegate = new ConfigurationDelegate() {
