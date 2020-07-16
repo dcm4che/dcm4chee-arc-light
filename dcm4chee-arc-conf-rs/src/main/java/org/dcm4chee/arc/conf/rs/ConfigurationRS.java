345097b84b815cec79bd120b6a7987d1c218fd22
Gunter Zeilinger <gunterze@protonmail.com>
2020-07-16 14:40:59 +0200

    Revert "Fix #2676 : RESTful service to list Web Applications: Return Keycloak realm and server URL"
    This reverts commit db2e4caa6851a8e7edd66bcdf4804a3a75719e91.

--- dcm4chee-arc-conf-rs/src/main/java/org/dcm4chee/arc/conf/rs/ConfigurationRS.java
+++ dcm4chee-arc-conf-rs/src/main/java/org/dcm4chee/arc/conf/rs/ConfigurationRS.java
@@ -198,7 +198,7 @@ public class ConfigurationRS {
                     JsonGenerator gen = Json.createGenerator(out);
                     gen.writeStartArray();
                     for (WebApplicationInfo webappInfo : webappInfos)
-                        jsonConf.writeTo(webappInfo, gen, keycloakClient(webappInfo));
+                        jsonConf.writeTo(webappInfo, gen, keycloakClientID(webappInfo));
                     gen.writeEnd();
                     gen.flush();
                 }).build();
@@ -207,13 +207,13 @@ public class ConfigurationRS {
         }
     }
 
-    private KeycloakClient keycloakClient(WebApplicationInfo webAppInfo) {
+    private String keycloakClientID(WebApplicationInfo webAppInfo) {
         String authURL = System.getProperty("auth-server-url");
         KeycloakClient keycloakClient = webAppInfo.getKeycloakClient();
         return keycloakClient == null || authURL == null
                 || (authURL.equals(keycloakClient.getKeycloakServerURL())
                     && System.getProperty("realm-name", "dcm4che").equals(keycloakClient.getKeycloakRealm()))
-                ? null : webAppInfo.getKeycloakClient();
+                ? null : webAppInfo.getKeycloakClientID();
     }
 
     @GET
