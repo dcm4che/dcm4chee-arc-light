ff507b7987dfe403cf39a309a282aa07a3757b22
Vrinda <vrinda.nayak@j4care.com>
2020-10-06 10:58:22 +0200

    Fix #2812 : Extend conditions: Support specifying individual value of multi-valued attribute by attribute-id['['number']']['!']'='pattern

--- dcm4chee-arc-conf/src/main/java/org/dcm4chee/arc/conf/Conditions.java
+++ dcm4chee-arc-conf/src/main/java/org/dcm4chee/arc/conf/Conditions.java
@@ -1,3 +1,43 @@
+/*
+ * *** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is part of dcm4che, an implementation of DICOM(TM) in
+ * Java(TM), hosted at https://github.com/dcm4che.
+ *
+ * The Initial Developer of the Original Code is
+ * J4Care.
+ * Portions created by the Initial Developer are Copyright (C) 2015-2020
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ * See @authors listed below
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * *** END LICENSE BLOCK *****
+ */
+
 package org.dcm4chee.arc.conf;
 
 import org.dcm4che3.data.Attributes;
@@ -10,6 +50,7 @@ import java.util.regex.Pattern;
 
 /**
  * @author Gunter Zeilinger <gunterze@gmail.com>
+ * @author Vrinda Nayak <vrinda.nayak@j4care.com>
  * @since Oct 2015
  */
 public class Conditions {
@@ -79,6 +120,12 @@ public class Conditions {
             boolean ne = tagPath.endsWith("!");
             if (ne)
                 tagPath = tagPath.substring(0, tagPath.length()-1);
+            int valPos = -1;
+            if (tagPath.endsWith("]")) {
+                String[] tagPathWithValPos = tagPath.split("\\[");
+                tagPath = tagPathWithValPos[0];
+                valPos = Integer.parseInt(tagPathWithValPos[1].substring(0, tagPathWithValPos[1].length() - 1));
+            }
             switch (tagPath) {
                 case RECEIVING_APPLICATION_ENTITY_TITLE:
                     if (ne ? (receivingAET != null && pattern.matcher(receivingAET).matches())
@@ -101,26 +148,31 @@ public class Conditions {
                         return false;
                     break;
                 default:
-                    if (!match(attrs, TagUtils.parseTagPath(tagPath), pattern, 0, ne))
+                    if (!match(attrs, TagUtils.parseTagPath(tagPath), pattern, 0, ne, valPos))
                         return false;
             }
         }
         return true;
     }
 
-    private boolean match(Attributes attrs, int[] tagPath, Pattern pattern, int level, boolean ne) {
+    private boolean match(Attributes attrs, int[] tagPath, Pattern pattern, int level, boolean ne, int valPos) {
         if (level < tagPath.length-1) {
             Sequence seq = attrs.getSequence(tagPath[level]);
             if (seq != null)
                 for (Attributes item : seq)
-                    if (match(item, tagPath, pattern, level+1, false))
+                    if (match(item, tagPath, pattern, level+1, false, valPos))
                         return !ne;
         } else {
             String[] ss = attrs.getStrings(tagPath[level]);
-            if (ss != null)
-                for (String s : ss)
-                    if (s != null && pattern.matcher(s).matches())
+            if (ss != null) {
+                if (valPos > -1) {
+                    if (valPos > ss.length || (ss[valPos - 1] != null && pattern.matcher(ss[valPos - 1]).matches()))
                         return !ne;
+                } else
+                    for (String s : ss)
+                        if (s != null && pattern.matcher(s).matches())
+                            return !ne;
+            }
         }
         return ne;
     }
