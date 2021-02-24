c136816335c7fb58644550e389d492d9318546ac
Vrinda <vrinda.nayak@j4care.com>
2021-02-24 09:26:52 +0100

    UI Navigation : Enable to show whole qualified Issuer of Patient of a patient record #2892

--- dcm4chee-arc-ui2/src/app/pipes/patient-issuer.pipe.spec.ts
+++ dcm4chee-arc-ui2/src/app/pipes/patient-issuer.pipe.spec.ts
@@ -1,6 +1,5 @@
 /* tslint:disable:no-unused-variable */
 
-import { TestBed, async } from '@angular/core/testing';
 import { PatientIssuerPipe } from './patient-issuer.pipe';
 
 describe('Pipe: PatientIssuer', () => {
