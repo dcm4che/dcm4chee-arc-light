673e1e8aee78de0c731ea4ad068c2242859a56f5
Vrinda <vrinda.nayak@j4care.com>
2021-04-19 12:30:41 +0200

    RESTful service to retrieve received MPPS #2968 : revert MPPS_RS web service class config changes

--- dcm4chee-arc-conf-test/src/test/java/org/dcm4chee/arc/conf/ArchiveDeviceFactory.java
+++ dcm4chee-arc-conf-test/src/test/java/org/dcm4chee/arc/conf/ArchiveDeviceFactory.java
@@ -1520,7 +1520,6 @@ class ArchiveDeviceFactory {
                 WebApplication.ServiceClass.UPS_RS,
                 WebApplication.ServiceClass.UPS_MATCHING,
                 WebApplication.ServiceClass.MWL_RS,
-                WebApplication.ServiceClass.MPPS_RS,
                 WebApplication.ServiceClass.DCM4CHEE_ARC_AET);
         device.addWebApplication(webapp);
         if (configType == configType.DOCKER) {
