959e6be9e91303328868741f15541a141dfd6a15
Shern <shern86@hotmail.com>
2021-05-24 23:50:24 +1000

    Fixes issues with MSSQL table creation

--- dcm4chee-arc-entity/src/main/resources/sql/create-case-insensitive-index.sql
+++ dcm4chee-arc-entity/src/main/resources/sql/create-case-insensitive-index.sql
@@ -1,4 +1,8 @@
-create index alphabetic_name_upper_idx on person_name (upper(alphabetic_name));
+ALTER TABLE person_name ADD upper_alphabetic_name AS UPPER(alphabetic_name)
+CREATE INDEX alphabetic_name_upper_idx ON person_name (upper_alphabetic_name);
 
-create index series_desc_upper_idx on series (upper(series_desc));
-create index study_desc_upper_idx on study (upper(study_desc));
+ALTER TABLE series ADD upper_series_desc AS UPPER(series_desc)
+CREATE INDEX series_desc_upper_idx ON series (upper_series_desc);
+	
+ALTER TABLE study ADD upper_study_desc AS UPPER(study_desc)
+CREATE INDEX study_desc_upper_idx ON person_name (upper_study_name);
