1745640a3aafafc2cc8a944b06a7af146f56623e
Vrinda <vrinda.nayak@j4care.com>
2021-09-24 10:58:24 +0200

    dcm4che/dcm4chee-arc-light#3380 : UI Navigation - Change Order of Tabs to Patients / Studies / Series / MWL / MPPS / Work Items / Compare

--- dcm4chee-arc-ui2/src/app/monitoring/monitoring-tabs.component.ts
+++ dcm4chee-arc-ui2/src/app/monitoring/monitoring-tabs.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
         <li [permission]="{id:'tab-monitoring->queues',param:'visible'}" role="presentation" routerLinkActive="active"><a role="tab" routerLink="/monitoring/queues" routerLinkActive="active" i18n="@@queues">Queues</a></li>
         <li [permission]="{id:'tab-monitoring->export',param:'visible'}" role="presentation" routerLinkActive="active"><a role="tab" routerLink="/monitoring/export" routerLinkActive="active" i18n="@@export.export">Export</a></li>
         <li [permission]="{id:'tab-monitoring->external_retrieve',param:'visible'}" role="presentation" routerLinkActive="active"><a role="tab" routerLink="/monitoring/external" routerLinkActive="active" i18n="@@retrieve">Retrieve</a></li>
-        <li [permission]="{id:'tab-monitoring->diff',param:'visible'}" role="presentation" routerLinkActive="active"><a role="tab" routerLink="/monitoring/diff" routerLinkActive="active" i18n="@@monitoring.tab.diff">Diff</a></li>
+        <li [permission]="{id:'tab-monitoring->diff',param:'visible'}" role="presentation" routerLinkActive="active"><a role="tab" routerLink="/monitoring/diff" routerLinkActive="active" i18n="@@monitoring.tab.diff">Compare</a></li>
         
         <!--<li role="presentation" routerLinkActive="active"><a role="tab" routerLink="/monitoring/statistics" routerLinkActive="active">Statistics</a></li>-->
         <!--// <li role="presentation" routerLinkActive="active"><a role="tab" routerLink="/monitoring/lifecycle-management" routerLinkActive="active">Lifecycle managements</a></li>-->
