cf9c41f5df00f5b30f8aff5d6cb532d41d2c1630
Vrinda <vrinda.nayak@j4care.com>
2021-12-09 16:36:46 +0100

    Revert "Fix #3479 : Remove redundant and unused AccessTokenWithExpiration.getAccessToken2(KeycloakClient keycloakClient)"
    This reverts commit 4db953b714b238405f6d0292ad2aabef3397ad78.

--- dcm4chee-arc-keycloak/src/main/java/org/dcm4chee/arc/keycloak/AccessTokenRequestor.java
+++ dcm4chee-arc-keycloak/src/main/java/org/dcm4chee/arc/keycloak/AccessTokenRequestor.java
@@ -110,6 +110,15 @@ public class AccessTokenRequestor {
                 accessToken.getExpiresIn());
     }
 
+    public AccessTokenWithExpiration getAccessToken2(KeycloakClient keycloakClient) throws Exception {
+        CachedKeycloak tmp = toCachedKeycloakClient(keycloakClient);
+        TokenManager tokenManager = tmp.keycloak.tokenManager();
+        AccessTokenResponse accessToken = tokenManager.getAccessToken();
+        return new AccessTokenWithExpiration(
+                accessToken.getToken(),
+                accessToken.getExpiresIn());
+    }
+
     private CachedKeycloak toCachedKeycloakClient(WebApplication webApp) throws Exception {
         CachedKeycloak tmp = cachedKeycloakClient;
         if (tmp == null || !tmp.keycloakID.equals(webApp.getKeycloakClientID())) {
