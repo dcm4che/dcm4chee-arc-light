a854df35dd469985525e0994c97e8ecbebc393cd
Vrinda <vrinda.nayak@j4care.com>
2022-07-29 10:14:22 +0200

    dcm4che/dcm4chee-arc-light#3743 : UI Navigation Series : Incorrect filters displayed in Schedule Storage Verification of matching Series widget

--- dcm4chee-arc-ui2/src/app/study/study/study.service.ts
+++ dcm4chee-arc-ui2/src/app/study/study/study.service.ts
@@ -4339,67 +4339,7 @@ export class StudyService {
             )
         }));
     }
-
-    storageVerificationSeriesSchema(){
-        return [
-            [
-                [
-                    {
-                        tag:"label",
-                        text:$localize `:@@patient_verification_status:Patient Verification Status`
-                    },
-                    {
-                        tag:"select",
-                        type:"text",
-                        options:[
-                            new SelectDropdown("UNVERIFIED", $localize `:@@UNVERIFIED:UNVERIFIED`),
-                            new SelectDropdown("VERIFIED", $localize `:@@VERIFIED:VERIFIED`),
-                            new SelectDropdown("NOT_FOUND", $localize `:@@NOT_FOUND:NOT_FOUND`),
-                            new SelectDropdown("VERIFICATION_FAILED", $localize `:@@VERIFICATION_FAILED:VERIFICATION_FAILED`)
-                        ],
-                        filterKey:"patientVerificationStatus",
-                        description:$localize `:@@patient_verification_status:Patient Verification Status`,
-                        placeholder:$localize `:@@status:Status`
-                    }
-                ],[
-                    {
-                        tag: "label",
-                        text: $localize`:@@all_of_modalities_in_study:All of Modalities in Study`
-                    },
-                    {
-                        tag:"checkbox",
-                        type:"text",
-                        filterKey:"allOfModalitiesInStudy",
-                        description:$localize `:@@all_of_modalities_in_study:All of Modalities in Study`
-                    },
-                ],
-                [
-                    {
-                        tag: "label",
-                        text: $localize`:@@schedule_at:Schedule at`
-                    },
-                    {
-                        tag:"single-date-time-picker",
-                        type:"text",
-                        filterKey:"scheduledTime",
-                        description:$localize `:@@schedule_at_desc:Schedule at (if not set, schedule immediately)`
-                    },
-                ], [
-                        {
-                            tag: "label",
-                            text: $localize`:@@batch_ID:Batch ID`
-                        },
-                        {
-                            tag: "input",
-                            type: "text",
-                            filterKey: "batchID",
-                            description: $localize`:@@batch_ID:Batch ID`,
-                            placeholder: $localize`:@@batch_ID:Batch ID`
-                        }
-                    ]
-            ]
-        ]
-    }
+    
     exportMatchingSeriesDialogSchema(exporterIDs){
         return [
             [
