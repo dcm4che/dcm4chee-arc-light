58e8093dc98454bda3d39cd5f092bebd6031a982
Gunter Zeilinger <gunterze@protonmail.com>
2023-07-12 15:03:06 +0200

    update-5.26-<db>.sql: several SQL statements are wrongly listed in part 2 instead of in part 1 fix #4130

--- dcm4chee-arc-entity/src/main/resources/sql/psql/update-5.26-psql.sql
+++ dcm4chee-arc-entity/src/main/resources/sql/psql/update-5.26-psql.sql
@@ -1,4 +1,4 @@
--- can be applied on archive running archive 5.25
+-- part 1: can be applied on archive running archive 5.25
 alter table series
     add receiving_hl7_app varchar(255),
     add receiving_hl7_facility varchar(255),
@@ -80,23 +80,6 @@ set (entity_id, entity_uid, entity_uid_type) =
          from issuer where issuer_fk = issuer.pk)
 where issuer_fk is not null;
 
-create index UK_ffpftwfkijejj09tlbxr7u5g8 on series (sending_hl7_app);
-create index UK_1e4aqxc5w1557hr3fb3lqm2qb on series (sending_hl7_facility);
-create index UK_gj0bxgi55bhjic9s3i4dp2aee on series (receiving_hl7_app);
-create index UK_pbay159cdhwbtjvlmel6d6em2 on series (receiving_hl7_facility);
-
-create index UK_tkyjkkxxhnr0fem7m0h3844jk on patient_id (pat_id);
-create index UK_d1sdyupb0vwvx23jownjnyy72 on patient_id (entity_id);
-create index UK_m2jq6xe87vegohf6g10t5ptew on patient_id (entity_uid, entity_uid_type);
-
-create index FK_q26e06qk9gwviwe2ug0f86doa on rel_ups_station_class_code (station_class_code_fk) ;
-create index FK_e1ioaswm010jlsq6kl7y3um1c on rel_ups_station_class_code (ups_fk) ;
-create index FK_kl60ab0k5c1p8qii9ya16424x on rel_ups_station_location_code (station_location_code_fk) ;
-create index FK_9f0l4glqwpq12d11w9osd475m on rel_ups_station_location_code (ups_fk) ;
-create index FK_jtv4r8f88f6gfte0fa36w5y9o on rel_ups_station_name_code (station_name_code_fk) ;
-create index FK_8jf5xe8ot2yammv3ksd5xrgif on rel_ups_station_name_code (ups_fk) ;
-
--- part 2: shall be applied on stopped archive before starting 5.26
 update mpps
 set (accno_entity_id, accno_entity_uid, accno_entity_uid_type) =
         (select issuer.entity_id, issuer.entity_uid, issuer.entity_uid_type
@@ -114,14 +97,6 @@ set (admid_entity_id, admid_entity_uid, admid_entity_uid_type) =
         (select issuer.entity_id, issuer.entity_uid, issuer.entity_uid_type
          from issuer where admid_issuer_fk = issuer.pk)
 where admid_issuer_fk is not null;
-
-update patient_id
-set (entity_id, entity_uid, entity_uid_type) =
-        (select issuer.entity_id, issuer.entity_uid, issuer.entity_uid_type
-         from issuer where issuer_fk = issuer.pk)
-where issuer_fk is not null
-  and entity_id is null
-  and entity_uid is null;
 
 update series_req
 set (accno_entity_id, accno_entity_uid, accno_entity_uid_type) =
@@ -152,6 +127,31 @@ set (accno_entity_id, accno_entity_uid, accno_entity_uid_type) =
         (select issuer.entity_id, issuer.entity_uid, issuer.entity_uid_type
          from issuer where accno_issuer_fk = issuer.pk)
 where accno_issuer_fk is not null;
+
+create index UK_ffpftwfkijejj09tlbxr7u5g8 on series (sending_hl7_app);
+create index UK_1e4aqxc5w1557hr3fb3lqm2qb on series (sending_hl7_facility);
+create index UK_gj0bxgi55bhjic9s3i4dp2aee on series (receiving_hl7_app);
+create index UK_pbay159cdhwbtjvlmel6d6em2 on series (receiving_hl7_facility);
+
+create index UK_tkyjkkxxhnr0fem7m0h3844jk on patient_id (pat_id);
+create index UK_d1sdyupb0vwvx23jownjnyy72 on patient_id (entity_id);
+create index UK_m2jq6xe87vegohf6g10t5ptew on patient_id (entity_uid, entity_uid_type);
+
+create index FK_q26e06qk9gwviwe2ug0f86doa on rel_ups_station_class_code (station_class_code_fk) ;
+create index FK_e1ioaswm010jlsq6kl7y3um1c on rel_ups_station_class_code (ups_fk) ;
+create index FK_kl60ab0k5c1p8qii9ya16424x on rel_ups_station_location_code (station_location_code_fk) ;
+create index FK_9f0l4glqwpq12d11w9osd475m on rel_ups_station_location_code (ups_fk) ;
+create index FK_jtv4r8f88f6gfte0fa36w5y9o on rel_ups_station_name_code (station_name_code_fk) ;
+create index FK_8jf5xe8ot2yammv3ksd5xrgif on rel_ups_station_name_code (ups_fk) ;
+
+-- part 2: shall be applied on stopped archive before starting 5.26
+update patient_id
+set (entity_id, entity_uid, entity_uid_type) =
+        (select issuer.entity_id, issuer.entity_uid, issuer.entity_uid_type
+         from issuer where issuer_fk = issuer.pk)
+where issuer_fk is not null
+  and entity_id is null
+  and entity_uid is null;
 
 insert into rel_ups_station_name_code
 select ups.pk, ups.station_name_fk from ups where ups.station_name_fk is not null;
