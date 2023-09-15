92bfeeb55f5f924100c3752953720ef358e4bb45
Gunter Zeilinger <gunterze@protonmail.com>
2023-09-15 17:41:49 +0200

    Provide dedicated binary distribution package for MariaDB fix #4224

---
+++ buildall.sh
@@ -0,0 +1,18 @@
+mvn install
+mvn install -Dsecure=all
+mvn install -Dsecure=ui
+mvn install -Ddb=mysql
+mvn install -Ddb=mysql -Dsecure=all
+mvn install -Ddb=mysql -Dsecure=ui
+mvn install -Ddb=mariadb
+mvn install -Ddb=mariadb -Dsecure=all
+mvn install -Ddb=mariadb -Dsecure=ui
+mvn install -Ddb=oracle
+mvn install -Ddb=oracle -Dsecure=all
+mvn install -Ddb=oracle -Dsecure=ui
+mvn install -Ddb=sqlserver
+mvn install -Ddb=sqlserver -Dsecure=all
+mvn install -Ddb=sqlserver -Dsecure=ui
+mvn install -Ddb=db2
+mvn install -Ddb=firebird
+mvn install -Ddb=h2
