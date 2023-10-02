e4c60edee1585b3d1d0ff6f51ff4da7605dabe45
Vrinda <vrinda.nayak@j4care.com>
2023-10-02 11:41:08 +0200

    RESTful service to verify DNS lookup #4242

---
+++ dcm4chee-arc-host-rs/src/main/java/org/dcm4chee/arc/host/rs/HostRS.java
@@ -0,0 +1,135 @@
+/*
+ * *** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is part of dcm4che, an implementation of DICOM(TM) in
+ * Java(TM), hosted at https://github.com/dcm4che.
+ *
+ * The Initial Developer of the Original Code is
+ * J4Care.
+ * Portions created by the Initial Developer are Copyright (C) 2015-2023
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ * See @authors listed below
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * *** END LICENSE BLOCK *****
+ */
+
+package org.dcm4chee.arc.host.rs;
+
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.json.Json;
+import jakarta.json.stream.JsonGenerator;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.StreamingOutput;
+import org.dcm4che3.conf.json.JsonWriter;
+import org.jboss.resteasy.annotations.cache.NoCache;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * @author Vrinda Nayak <vrinda.nayak@j4care.com>
+ * @since Sep 2023
+ */
+
+@RequestScoped
+@Path("/host")
+public class HostRS {
+    private static final Logger LOG = LoggerFactory.getLogger(HostRS.class);
+
+    @Context
+    private HttpServletRequest request;
+
+    @GET
+    @NoCache
+    @Path("/{host}")
+    @Produces("application/json")
+    public Response listHosts(@PathParam("host") String host) {
+        try {
+            long startInetAddrAll= System.currentTimeMillis();
+            InetAddress[] inetAddresses = InetAddress.getAllByName(host);
+            return Response.ok((StreamingOutput) out -> {
+                JsonGenerator gen = Json.createGenerator(out);
+                JsonWriter writer = new JsonWriter(gen);
+                writer.writeStartObject();
+                writer.writeStartArray("hosts");
+                for (InetAddress inetAddress : inetAddresses) {
+                    writer.writeStartObject();
+                    long startInetAddrHostName = System.currentTimeMillis();
+                    writer.writeNotNullOrDef("name", inetAddress.getHostName(), null);
+                    long endInetAddrHostName = System.currentTimeMillis();
+                    writer.writeNotNullOrDef("address", inetAddress.getHostAddress(), null);
+                    writer.writeNotDef("rDNSTime", endInetAddrHostName - startInetAddrHostName, 0);
+                    writer.writeEnd();
+                }
+                writer.writeEnd();
+                long endInetAddrAll= System.currentTimeMillis();
+                writer.writeNotDef("DNSTime", endInetAddrAll - startInetAddrAll, 0);
+                writer.writeEnd();
+                gen.flush();
+            }).build();
+        } catch (UnknownHostException e) {
+            return errResponse("No IP address for the host could be found: " + host, Response.Status.NOT_FOUND);
+        } catch (Exception e) {
+            return errResponseAsTextPlain(exceptionAsString(e), Response.Status.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    private void logRequest() {
+        LOG.info("Process GET {} from {}@{}",
+                request.getRequestURI(),
+                request.getRemoteUser(),
+                request.getRemoteHost());
+    }
+
+    private Response errResponse(String errorMessage, Response.Status status) {
+        return errResponseAsTextPlain("{\"errorMessage\":\"" + errorMessage + "\"}", status);
+    }
+
+    private Response errResponseAsTextPlain(String errorMsg, Response.Status status) {
+        return Response.status(status)
+                .entity(errorMsg)
+                .type("text/plain")
+                .build();
+    }
+
+    private String exceptionAsString(Exception e) {
+        StringWriter sw = new StringWriter();
+        e.printStackTrace(new PrintWriter(sw));
+        return sw.toString();
+    }
+}
