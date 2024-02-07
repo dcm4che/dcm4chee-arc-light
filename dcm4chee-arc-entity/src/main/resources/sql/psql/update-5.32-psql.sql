7f70204cb715d57bae8c3ff3831823f989a71fdf
Vrinda <vrinda.nayak@j4care.com>
2024-02-07 16:27:08 +0100

    Replace null by * for field externalRetrieveAET of entity Instance and Series and change corresponding Nullable column ext_retrieve_aet to NOT NULL #4402

---
+++ dcm4chee-arc-entity/src/main/resources/sql/psql/update-5.32-psql.sql
@@ -0,0 +1,7 @@
+-- can be applied on archive running archive 5.31
+
+-- part 2: shall be applied on stopped archive before starting 5.32
+update instance set ext_retrieve_aet = '*' where ext_retrieve_aet is null;
+update series set ext_retrieve_aet = '*' where ext_retrieve_aet is null;
+
+-- part 3: can be applied on already running archive 5.32
