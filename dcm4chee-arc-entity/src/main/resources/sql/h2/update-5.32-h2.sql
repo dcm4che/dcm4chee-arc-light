35ce4a3e769699e3a854fdf8599c38c28b8f258e
Vrinda <vrinda.nayak@j4care.com>
2024-04-18 15:14:09 +0200

    Update DB SQL scripts for 5.32.0 for databases other than postgres #4422

--- dcm4chee-arc-entity/src/main/resources/sql/h2/update-5.32-h2.sql
+++ dcm4chee-arc-entity/src/main/resources/sql/h2/update-5.32-h2.sql
@@ -1,11 +1,15 @@
 -- can be applied on archive running archive 5.31
 update instance set ext_retrieve_aet = '*' where ext_retrieve_aet is null;
 update series set ext_retrieve_aet = '*' where ext_retrieve_aet is null;
+alter table study add deleting boolean;
+update study set deleting = false;
 
 -- part 2: shall be applied on stopped archive before starting 5.32
 update instance set ext_retrieve_aet = '*' where ext_retrieve_aet is null;
 update series set ext_retrieve_aet = '*' where ext_retrieve_aet is null;
+update study set deleting = false where deleting is null;
 
 -- part 3: can be applied on already running archive 5.32
 alter table instance alter column ext_retrieve_aet set not null;
 alter table series alter column ext_retrieve_aet set not null;
+alter table study alter deleting set not null;
