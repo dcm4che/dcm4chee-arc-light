8a742b06243e570b151b966398002505e8bcfd78
Vrinda <vrinda.nayak@j4care.com>
2024-04-19 10:14:13 +0200

    dcm4che/dcm4chee-arc-light#4482 : Rename ParticipantObjectIDTypeCode for Query audits triggered by REST

--- dcm4chee-arc-audit/src/main/java/org/dcm4chee/arc/audit/QueryAuditService.java
+++ dcm4chee-arc-audit/src/main/java/org/dcm4chee/arc/audit/QueryAuditService.java
@@ -109,7 +109,7 @@ class QueryAuditService extends AuditService {
     static ParticipantObjectIdentification qidoQuery(AuditInfo auditInfo) {
         ParticipantObjectIdentification poi = new ParticipantObjectIdentification();
         poi.setParticipantObjectID(auditInfo.getField(AuditInfo.Q_POID));
-        poi.setParticipantObjectIDTypeCode(AuditMessages.ParticipantObjectIDTypeCode.QIDO_QUERY);
+        poi.setParticipantObjectIDTypeCode(AuditMessages.ParticipantObjectIDTypeCode.REST);
         poi.setParticipantObjectTypeCode(AuditMessages.ParticipantObjectTypeCode.SystemObject);
         poi.setParticipantObjectTypeCodeRole(AuditMessages.ParticipantObjectTypeCodeRole.Query);
         poi.getParticipantObjectDetail()
