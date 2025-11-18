9d1c2e35029f3754d5a757d96277ee99e9b0a288
Vrinda <vrinda.nayak@j4care.com>
2025-11-18 13:30:22 +0100

    Fix #4843 : Enable different value of Result Status in HL7 Procedure Status Update Notifications to differentiate FIRST_OBJECT_OF_STUDY_RECEIVED notification from that of STUDY_RECEIVED

--- dcm4chee-arc-hl7-psu/src/main/java/org/dcm4chee/arc/hl7/psu/HL7PSUEJB.java
+++ dcm4chee-arc-hl7-psu/src/main/java/org/dcm4chee/arc/hl7/psu/HL7PSUEJB.java
@@ -83,6 +83,7 @@ public class HL7PSUEJB {
     private static final String HL7PSU_MSG_ABORT = "Send HL7 Procedure Status Update notification aborted. ";
 
     //HL7v2.5 Ch4 OBR-25 ResultStatus
+    private static final String HL7PSU_RESULTS_STORED_PRELIMINARY = "P";
     private static final String HL7PSU_RESULTS_NOT_AVAILABLE = "S";
     private static final String HL7PSU_RESULTS_STORED = "R";
     private static final String HL7PSU_PARTIAL_REJECT = "A";
@@ -274,7 +275,7 @@ public class HL7PSUEJB {
             case COMPLETE:
                 return HL7PSU_COMPLETE_REJECT;
             default:
-                return HL7PSU_RESULTS_STORED;
+                return HL7PSU_RESULTS_STORED_PRELIMINARY;
         }
     }
 
