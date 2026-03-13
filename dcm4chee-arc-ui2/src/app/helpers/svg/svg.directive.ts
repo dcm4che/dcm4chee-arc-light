0b7902f07f162886c6d1d7669b87cb4a374c166a
Shefki Esadi <shralsheki@gmail.com>
2026-03-13 17:11:28 +0100

    UI: Extend svg directive to be able to use svg files #4909

--- dcm4chee-arc-ui2/src/app/helpers/svg/svg.directive.ts
+++ dcm4chee-arc-ui2/src/app/helpers/svg/svg.directive.ts
@@ -1,4 +1,6 @@
 import {Directive, ElementRef, Input, OnInit} from '@angular/core';
+import {J4careHttpService} from "../j4care-http.service";
+import * as _ from 'lodash-es';
 
 @Directive({
     selector: '[svg]',
@@ -8,21 +10,52 @@ export class SvgDirective implements OnInit{
     @Input() svg: string;
     @Input() svgWidth: string;
     @Input() svgHeight: string;
+    @Input() svgPath: string;
      parser = new DOMParser();
+    private static cache = new Map<string, string>();
 
   constructor(
       private el: ElementRef,
+      private $http:J4careHttpService
   ) {
 
   }
 
-    ngOnInit(): void {
+    async ngOnInit(): Promise<void> {
         if (this.svg && this.svg.trim().substring(0, 4) === "<svg" && this.svg.trim().substring(this.svg.trim().length - 6) === "</svg>"){
-            this.createSvg();
+            this.createSvg(this.svg);
+        }
+        if(this.svgPath){
+            await this.loadSvgFromPath();
         }
     }
-  private createSvg(){
-      let img = this.parser.parseFromString(this.svg, 'image/svg+xml');
+    private async loadSvgFromPath() {
+
+        let svgText: string;
+
+        if (SvgDirective.cache.has(this.svgPath)) {
+            svgText = SvgDirective.cache.get(this.svgPath)!;
+        } else {
+            svgText = await this.$http
+                .get(this.svgPath, { responseType: 'text' })
+                .toPromise();
+            if (svgText && _.has(svgText, 'body') && _.has(svgText, 'status') && svgText["status"] === 200) {
+                svgText = svgText["body"];
+            }
+            SvgDirective.cache.set(this.svgPath, svgText);
+        }
+
+        if (this.isValidSvg(svgText)) {
+            this.createSvg(svgText);
+        }
+    }
+    private isValidSvg(svg: string): boolean {
+        const s = svg.trim();
+        return s.startsWith('<svg') && s.endsWith('</svg>');
+    }
+
+    private createSvg(svgText?: string): void {
+      let img = this.parser.parseFromString(svgText, 'image/svg+xml');
       if(this.svgWidth){
         img.documentElement.setAttribute("width",this.svgWidth);
       }
