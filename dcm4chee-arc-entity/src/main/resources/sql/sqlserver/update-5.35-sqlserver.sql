fab04ef69b0e1e506267776a360b9c06f2eee964
Vrinda <vrinda.nayak@j4care.com>
2026-05-21 12:33:30 +0200

    dcm4che/dcm4chee-arc-light#4953 : Update DB SQL scripts for 5.35.0 for databases other than postgres

--- dcm4chee-arc-entity/src/main/resources/sql/sqlserver/update-5.35-sqlserver.sql
+++ dcm4chee-arc-entity/src/main/resources/sql/sqlserver/update-5.35-sqlserver.sql
@@ -1,11 +1,33 @@
 -- part 1: can be applied on archive running archive 5.34
 create table study_access_control_id (study_fk bigint not null, access_control_id varchar(255));
-alter table if exists study_access_control_id add constraint FKluah7q5kdwqidu6uwlrsndd7k foreign key (study_fk) references study;
+IF OBJECT_ID('dbo.study_access_control_id', 'U') IS NOT NULL
+AND NOT EXISTS (
+    SELECT 1
+    FROM sys.foreign_keys
+    WHERE name = 'FKluah7q5kdwqidu6uwlrsndd7k'
+)
+BEGIN
+ALTER TABLE dbo.study_access_control_id
+    ADD CONSTRAINT FKluah7q5kdwqidu6uwlrsndd7k
+        FOREIGN KEY (study_fk)
+            REFERENCES dbo.study(pk);
+END
 
 create table series_access_control_id (series_fk bigint not null, access_control_id varchar(255));
-alter table if exists series_access_control_id add constraint FKt7uu3btv6pro4wuxspq0pom8y foreign key (series_fk) references series;
+IF OBJECT_ID('dbo.series_access_control_id', 'U') IS NOT NULL
+AND NOT EXISTS (
+    SELECT 1
+    FROM sys.foreign_keys
+    WHERE name = 'FKt7uu3btv6pro4wuxspq0pom8y'
+)
+BEGIN
+ALTER TABLE dbo.series_access_control_id
+    ADD CONSTRAINT FKt7uu3btv6pro4wuxspq0pom8y
+        FOREIGN KEY (series_fk)
+            REFERENCES dbo.series(pk);
+END
 
-alter table series add num_instances integer;
+alter table series add num_instances int;
 
 -- part 2: shall be applied on stopped archive before starting 5.35
 insert into study_access_control_id(study_fk, access_control_id)
@@ -20,9 +42,9 @@ from series where access_control_id != '*';
 create index IDXmt8f5xaiqgiwps1j4riuge3lm on series_access_control_id (access_control_id);
 create index FKt7uu3btv6pro4wuxspq0pom8y on series_access_control_id (series_fk) ;
 
-alter table study alter access_control_id drop not null;
-alter table series alter access_control_id drop not null;
+alter table study alter column access_control_id varchar(255) null;
+alter table series alter column access_control_id varchar(255) null;
 
 -- part 3: can be applied on already running archive 5.35
-drop index IDX24av2ewa70e7cykl340n63aqd;
-drop index IDXr9qbr5jv4ejclglvyvtsynuo9;
+drop index IDX24av2ewa70e7cykl340n63aqd on study;
+drop index IDXr9qbr5jv4ejclglvyvtsynuo9 on series;
