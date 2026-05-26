705f870dd2a7bbcbfed17dd382edab57aae5300f
Vrinda <vrinda.nayak@j4care.com>
2026-05-26 09:19:16 +0200

    dcm4che/dcm4chee-arc-light#4953 : Update DB SQL scripts for 5.35.0 for databases other than postgres

---
+++ dcm4chee-arc-entity/src/main/resources/sql/firebird/update-5.35-firebird.sql
@@ -0,0 +1,44 @@
+-- part 1: can be applied on archive running archive 5.34
+create table study_access_control_id (study_fk bigint not null, access_control_id varchar(255));
+SELECT COUNT(*)
+FROM RDB$RELATION_CONSTRAINTS
+WHERE RDB$CONSTRAINT_NAME = 'FKLUAH7Q5KDWQIDU6UWLRSNDD7K';
+--if returns 0, execute next
+ALTER TABLE study_access_control_id
+    ADD CONSTRAINT FKluah7q5kdwqidu6uwlrsndd7k
+        FOREIGN KEY (study_fk)
+            REFERENCES study(pk);
+
+create table series_access_control_id (series_fk bigint not null, access_control_id varchar(255));
+SELECT COUNT(*)
+FROM RDB$RELATION_CONSTRAINTS
+WHERE RDB$CONSTRAINT_NAME = 'FKt7uu3btv6pro4wuxspq0pom8y';
+--if returns 0, execute next
+ALTER TABLE series_access_control_id
+    ADD CONSTRAINT FKt7uu3btv6pro4wuxspq0pom8y
+        FOREIGN KEY (series_fk)
+            REFERENCES series(pk);
+
+alter table series add num_instances integer;
+
+-- part 2: shall be applied on stopped archive before starting 5.35
+insert into study_access_control_id(study_fk, access_control_id)
+select study.pk, study.access_control_id
+from study where access_control_id != '*';
+create index IDXl3g07pncx4agpayus50wn8u40 on study_access_control_id (access_control_id);
+-- don't execute below, else throws already exists
+-- create index FKluah7q5kdwqidu6uwlrsndd7k on study_access_control_id (study_fk) ;
+
+insert into series_access_control_id(series_fk, access_control_id)
+select series.pk, series.access_control_id
+from series where access_control_id != '*';
+create index IDXmt8f5xaiqgiwps1j4riuge3lm on series_access_control_id (access_control_id);
+-- don't execute below, else throws already exists
+-- create index FKt7uu3btv6pro4wuxspq0pom8y on series_access_control_id (series_fk) ;
+
+alter table study alter access_control_id drop not null;
+alter table series alter access_control_id drop not null;
+
+-- part 3: can be applied on already running archive 5.35
+drop index IDX24av2ewa70e7cykl340n63aqd;
+drop index IDXr9qbr5jv4ejclglvyvtsynuo9;
