f2269b8a8b6d1121ebcd7a954db6c4f721897e4a
Shefki Esadi <shralsheki@gmail.com>
2026-07-20 17:04:56 +0200

    UI: Fixing unit test after upgrade to Angular 22  #5016

--- dcm4chee-arc-ui2/src/app/helpers/svg/svg.directive.spec.ts
+++ dcm4chee-arc-ui2/src/app/helpers/svg/svg.directive.spec.ts
@@ -3,7 +3,8 @@ import { SvgDirective } from './svg.directive';
 describe('SvgDirective', () => {
   it('should create an instance', () => {
     const mockElementRef = {} as any;
-    const directive = new SvgDirective(mockElementRef);
+    const mockHttp = {} as any;
+    const directive = new SvgDirective(mockElementRef, mockHttp);
     expect(directive).toBeTruthy();
   });
 });
